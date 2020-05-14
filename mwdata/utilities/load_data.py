695d54d69d83895ab79a0c38ae3b87f24bfd318b
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-05-14 15:25:51 -0500

    Change kwargs=None to **kwargs and used kwargs.get

--- mwdata/utilities/load_data.py
+++ mwdata/utilities/load_data.py
@@ -4,12 +4,14 @@ import pandas as pd
 GEO_EXTENSIONS = ['.dbf', '.shp', '.shx']
 
 
-def load_data(filepath, all_folders=False, kwargs=None):
-    """ Create pandas data frame from filepath
+def load_data(filepath, all_folders=False, **kwargs):
+    """Create pandas data frame from filepath
 
     Args:
-        filepath: The file path. Can be either a local filepath or Google Cloud Storage URI filepath
-        all_folders: If True, searches for text files in nested folders. If False, looks for text files in the current folder
+        filepath: The file path. Can be either a local filepath or
+        Google Cloud Storage URI filepath
+        all_folders: If True, searches for text files in nested folders.
+        If False, looks for text files in the current folder
         kwargs: Keyword arguments to pass to the reader
             .shp: Uses geopandas.read_file
             .csv, .json, and other: Uses pandas.read_csv or pandas.read_json
@@ -17,13 +19,11 @@ def load_data(filepath, all_folders=False, kwargs=None):
     Returns:
         A pandas data frame
     """
-    if os.path.isfile(filepath) or 'gs://' in filepath :
-        df = read_file_type(filepath, kwargs=kwargs)
+    if os.path.isfile(filepath) or 'gs://' in filepath:
+        df = read_file_type(filepath, **kwargs)
     elif os.path.isdir(filepath):
         text = []
-        if kwargs is None:
-            kwargs = {}
-        encoding = kwargs.pop('encoding', 'utf-8')
+        encoding = kwargs.get('encoding', 'utf-8')
         if not all_folders:
             for file in os.listdir(filepath):
                 if os.path.isfile(os.path.join(filepath, file)) and file.endswith(".txt"):
@@ -41,8 +41,8 @@ def load_data(filepath, all_folders=False, kwargs=None):
     return df
 
 
-def read_file_type(filepath, kwargs=None):
-    """ Read the file based on file extension
+def read_file_type(filepath, **kwargs):
+    """Read the file based on file extension
 
     Currently supports the following filetypes:
         csv, json, txt, shp
@@ -56,44 +56,38 @@ def read_file_type(filepath, kwargs=None):
     """
     extension = os.path.splitext(filepath)[1]
     if extension == '.csv':
-        if kwargs is None:
-            kwargs = {}
         return pd.read_csv(filepath, **kwargs)
     elif extension == '.json':
-        if kwargs is None:
-            kwargs = {'lines': True}
-        return pd.read_json(filepath, **kwargs)
+        lines = kwargs.pop('lines', True)
+        return pd.read_json(filepath, lines=lines, **kwargs)
     elif extension == '.shp':
         import geopandas as gpd
         import re
-        if kwargs is None:
-            kwargs = {}
         if 'gs://' in filepath:
             bucket = re.search(r'(?<=gs://)[\w-]*', filepath).group(0)
             file = re.search(r'/[\w-]*.shp', filepath).group(0)
             prefix = re.search(r'{0}.*/'.format(bucket), filepath).group(0)
             for ext in GEO_EXTENSIONS:
-                fpath = download_gcs_file(file + ext, bucket=bucket, prefix=prefix, kwargs=kwargs)
+                fpath = download_gcs_file(
+                    file + ext, bucket=bucket, prefix=prefix, kwargs=kwargs)
             return gpd.read_file(fpath, **kwargs)
         else:
             return gpd.read_file(filepath, **kwargs)
     elif extension == '.xlsx':
-        if kwargs is None:
-            kwargs = {}
         return pd.read_excel(filepath, **kwargs)
     else:
-        if kwargs is None:
-            kwargs = {'sep': '\n'}
-        return pd.read_csv(filepath, **kwargs)
+        sep = kwargs.pop('sep', '\n')
+        return pd.read_csv(filepath, sep, **kwargs)
 
 
-def download_gcs_file(filepath, bucket=None, prefix=None, kwargs=None):
-    """ Downloads files from Google Cloud Storage
+def download_gcs_file(filepath, bucket=None, prefix=None, **kwargs):
+    """Downloads files from Google Cloud Storage
 
     Args:
         filepath: The file path
         bucket: bucket to which the file belongs to in Google Cloud Storage
-        prefix: parameter in list_blobs to limit the results to objects that have the specified prefix
+        prefix: parameter in list_blobs to limit the results to objects
+        that have the specified prefix
         kwargs: Keyword arguments for
     Returns:
 
@@ -102,9 +96,7 @@ def download_gcs_file(filepath, bucket=None, prefix=None, kwargs=None):
     import tempfile
     client = storage.Client()
     bucket = client.bucket(bucket)
-    if kwargs is None:
-        kwargs = {}
-    max_results = kwargs.pop('max_results', None)
+    max_results = kwargs.get('max_results')
     blobs = bucket.list_blobs(prefix=prefix, max_results=max_results)
     tmpdir = tempfile.gettempdir()
     shapefile_dir = None
