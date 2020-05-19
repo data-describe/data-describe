9ab2e624fdf10bda7bc3a2d6a119107c0dc2bdd7
David Law <dvdlaw@gmail.com>
2020-05-18 20:30:18 -0500

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,144 +1,57 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
-
-import io
-import os
-import sys
-from shutil import rmtree
-
-from setuptools import find_packages, setup, Command
+from setuptools import find_packages, setup
 
 # Package meta-data.
-NAME = 'mwdata'
-DESCRIPTION = 'Maven Wave Data Describe'
-URL = 'https://bitbucket.org/mavenwave/mw-data_describe'
-EMAIL = 'ml-na@mavenwave.com'
-AUTHOR = 'David Law, Richard Truong-Chau, Ross Claytor, Akanksha Jindal'
-REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.2a'
+NAME = "mwdata"
+DESCRIPTION = "Data Describe"
+URL = "https://github.com/brianray/data-describe"
+EMAIL = ""
+AUTHOR = "David Law, Richard Truong-Chau, Ross Claytor, Akanksha Jindal"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "0.2a"
 
-# What packages are required for this module to be executed?
 REQUIRED = [
-    'pandas>=0.24.0',
-    'numpy>=1.15.4',
-    'scipy>=1.1.0',
-    'seaborn>=0.9.0',
-    'scikit-learn>=0.20.3',
-    'eli5>=0.8.1',
-    'networkx>=2.2',
-    'hdbscan>=0.8.20',
-    'gcsfs>=0.2.1',
-    'plotly>=3.8.1',
-    'google-cloud-storage>=1.18.0'
+    "pandas>=0.24.0",
+    "numpy>=1.15.4",
+    "scipy>=1.1.0",
+    "seaborn>=0.9.0",
+    "scikit-learn>=0.20.3",
+    "eli5>=0.8.1",
+    "networkx>=2.2",
+    "hdbscan>=0.8.20",
+    "gcsfs>=0.2.1",
+    "plotly>=3.8.1",
+    "google-cloud-storage>=1.18.0",
 ]
 
-# What packages are optional?
 EXTRAS = {
-    'geo': ["geopandas>=0.5.0", "shapely>=1.6.4", "geoplot>=0.2.4", "descartes>=1.1.0"],
-    'nlp': ["nltk>=3.4", "pyldavis>=2.1.2", "gensim>=3.4.0"],
-    'gcp': ["gcsfs>=0.2.1"],
-    'excel': ["xlrd>=1.2.0"]
+    "geo": ["geopandas>=0.5.0", "shapely>=1.6.4", "geoplot>=0.2.4", "descartes>=1.1.0"],
+    "nlp": ["nltk>=3.4", "pyldavis>=2.1.2", "gensim>=3.4.0"],
+    "gcp": ["gcsfs>=0.2.1"],
+    "excel": ["xlrd>=1.2.0"],
 }
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
 
 
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=VERSION,
     description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
 )
