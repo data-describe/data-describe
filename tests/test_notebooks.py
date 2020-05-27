e7aa211839a0bfdd98c42dbb233b2c416f0ec834
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-05-27 16:03:13 -0500

    Remove result

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -11,70 +11,70 @@ fixture = NBRegressionFixture(exec_timeout=120, diff_color_words=True, diff_igno
 
 def test_geospatial_analysis_notebook():
     with importlib_resources.path(package=notebooks, resource='Geospatial Analysis.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_model_evaluation_notebook():
     with importlib_resources.path(package=notebooks, resource='Model Evaluation.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_data_loader_notebook():
     with importlib_resources.path(package=notebooks, resource='Data Loader.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_text_preprocessing_notebook():
     with importlib_resources.path(package=notebooks, resource='Text Preprocessing.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_cluster_analysis_notebook():
     with importlib_resources.path(package=notebooks, resource='Cluster Analysis.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_correlation_matrix_notebook():
     with importlib_resources.path(package=notebooks, resource='Correlation Matrix.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_data_heatmap_notebook():
     with importlib_resources.path(package=notebooks, resource='Data Heatmap.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_feature_importance_notebook():
     with importlib_resources.path(package=notebooks, resource='Feature Importance.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_distributions_notebook():
     with importlib_resources.path(package=notebooks, resource='Distributions.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_topic_model_notebook():
     with importlib_resources.path(package=notebooks, resource='Topic Modeling.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_scatter_plots_notebook():
     with importlib_resources.path(package=notebooks, resource='Scatter Plots.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 def test_data_summary_notebook():
     fixture = NBRegressionFixture(exec_timeout=120, diff_color_words=True, diff_ignore=('/cells/4/outputs/0/text',))
     with importlib_resources.path(package=notebooks, resource='Data Summary.ipynb') as path:
-        result = fixture.check(str(path), raise_errors=True)
+        fixture.check(str(path), raise_errors=True)
 
 
 # def test_tutorial_notebook():
 #     with importlib_resources.path(package=notebooks, resource='Tutorial.ipynb') as path:
-#         result = fixture.check(str(path), raise_errors=True)
+#         fixture.check(str(path), raise_errors=True)
 
 
 # def test_auto_data_type_notebook():
 #     with importlib_resources.path(package=notebooks, resource='Auto Data Type.ipynb') as path:
-#         result = fixture.check(str(path), raise_errors=True)
+#         fixture.check(str(path), raise_errors=True)
