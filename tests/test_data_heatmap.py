5504d3350048b9a363a14ed25b0aec148f093674
David Law <dvdlaw@gmail.com>
2020-06-01 09:57:18 -0500

    Unit test data (Resolves #60) (#78)

--- tests/test_data_heatmap.py
+++ tests/test_data_heatmap.py
@@ -1,38 +1,39 @@
-# import pandas as pd
-# import mwdata as mw
-# import matplotlib
-# import pytest
+import matplotlib
+import pytest
 
-# matplotlib.use("Agg")
+from ._test_data import DATA
+import mwdata as mw
 
-
-# @pytest.fixture
-# def data():
-#     return pd.read_csv("data/weatherAUS.csv")
+matplotlib.use("Agg")
 
 
-# def test_heatmap(data):
-#     with pytest.raises(OSError):
-#         mw.data_heatmap(data)
+@pytest.fixture
+def data():
+    return DATA
 
 
-# def test_heatmap_static(data):
-#     fig = mw.data_heatmap(data, interactive=False)
-#     assert isinstance(fig, matplotlib.artist.Artist)
+def test_heatmap(data):
+    with pytest.raises(OSError):
+        mw.data_heatmap(data)
 
 
-# def test_heatmap_missing(data):
-#     fig = mw.data_heatmap(data, missing=True, interactive=False)
-#     assert isinstance(fig, matplotlib.artist.Artist)
+def test_heatmap_static(data):
+    fig = mw.data_heatmap(data, interactive=False)
+    assert isinstance(fig, matplotlib.artist.Artist)
 
 
-# def test_heatmap_numpy(data):
-#     data = data.select_dtypes(["number"]).to_numpy()
-#     fig = mw.data_heatmap(data, interactive=False)
-#     assert isinstance(fig, matplotlib.artist.Artist)
+def test_heatmap_missing(data):
+    fig = mw.data_heatmap(data, missing=True, interactive=False)
+    assert isinstance(fig, matplotlib.artist.Artist)
 
 
-# def test_heatmap_invalid():
-#     data = [1, 2, 3, 4]
-#     with pytest.raises(ValueError):
-#         mw.data_heatmap(data)
+def test_heatmap_numpy(data):
+    data = data.select_dtypes(["number"]).to_numpy()
+    fig = mw.data_heatmap(data, interactive=False)
+    assert isinstance(fig, matplotlib.artist.Artist)
+
+
+def test_heatmap_invalid():
+    data = [1, 2, 3, 4]
+    with pytest.raises(ValueError):
+        mw.data_heatmap(data)
--- tests/test_dimensionality_reduction.py
+++ tests/test_dimensionality_reduction.py
@@ -1,41 +1,38 @@
-# import pytest
-# import pandas as pd
-# import sklearn
-# from sklearn.datasets import load_wine
+import pytest
+import sklearn
+import pandas as pd
+import numpy as np
 
-# from mwdata.dimensionality_reduction.dimensionality_reduction import dim_reduc
-
-
-# @pytest.fixture
-# def data_loader():
-#     data = load_wine()
-#     df = pd.DataFrame(data=data.data, columns=data.feature_names)
-#     df = pd.concat([pd.Series(data.target), df], axis=1)
-#     df = df.rename({0: "Target"}, axis=1)
-#     return df.sample(n=50, replace=True, random_state=1)
+from ._test_data import DATA
+from mwdata.dimensionality_reduction.dimensionality_reduction import dim_reduc
 
 
-# def test_error():
-#     with pytest.raises(NotImplementedError):
-#         dim_reduc(data=data_loader, n_components=2, dim_method="test_dim_method")
+@pytest.fixture
+def data():
+    return DATA.select_dtypes(np.number)
 
 
-# def test_pca(data_loader):
-#     x = dim_reduc(data=data_loader, n_components=2, dim_method="pca")
-#     assert isinstance(x, tuple)
-#     assert isinstance(x[0], pd.core.frame.DataFrame)
-#     assert isinstance(x[1], sklearn.decomposition.pca.PCA)
+def test_error():
+    with pytest.raises(NotImplementedError):
+        dim_reduc(data=data, n_components=2, dim_method="test_dim_method")
 
 
-# def test_tsne(data_loader):
-#     x = dim_reduc(data=data_loader, n_components=2, dim_method="tsne")
-#     assert isinstance(x, tuple)
-#     assert isinstance(x[0], pd.core.frame.DataFrame)
-#     assert isinstance(x[1], sklearn.manifold.t_sne.TSNE)
+def test_pca(data):
+    x = dim_reduc(data=data, n_components=2, dim_method="pca")
+    assert isinstance(x, tuple)
+    assert isinstance(x[0], pd.core.frame.DataFrame)
+    assert isinstance(x[1], sklearn.decomposition.PCA)
 
 
-# def test_tsvd(data_loader):
-#     x = dim_reduc(data=data_loader, n_components=2, dim_method="tsvd")
-#     assert isinstance(x, tuple)
-#     assert isinstance(x[0], pd.core.frame.DataFrame)
-#     assert isinstance(x[1], sklearn.decomposition.truncated_svd.TruncatedSVD)
+def test_tsne(data):
+    x = dim_reduc(data=data, n_components=2, dim_method="tsne")
+    assert isinstance(x, tuple)
+    assert isinstance(x[0], pd.core.frame.DataFrame)
+    assert isinstance(x[1], sklearn.manifold.TSNE)
+
+
+def test_tsvd(data):
+    x = dim_reduc(data=data, n_components=2, dim_method="tsvd")
+    assert isinstance(x, tuple)
+    assert isinstance(x[0], pd.core.frame.DataFrame)
+    assert isinstance(x[1], sklearn.decomposition.TruncatedSVD)
