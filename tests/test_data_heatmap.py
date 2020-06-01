070a7df6a8c4c5f7571b1e69facde3823a81caaa
David Law <dvdlaw@gmail.com>
2020-05-31 21:13:57 -0500

    Fix data heatmap unit tests

--- tests/test_data_heatmap.py
+++ tests/test_data_heatmap.py
@@ -1,38 +1,46 @@
-# import pandas as pd
-# import mwdata as mw
-# import matplotlib
-# import pytest
+import matplotlib
+import pytest
+import pandas as pd
+import numpy as np
 
-# matplotlib.use("Agg")
+import mwdata as mw
 
-
-# @pytest.fixture
-# def data():
-#     return pd.read_csv("data/weatherAUS.csv")
+matplotlib.use("Agg")
 
 
-# def test_heatmap(data):
-#     with pytest.raises(OSError):
-#         mw.data_heatmap(data)
+@pytest.fixture
+def data():
+    df = pd.DataFrame(
+        {
+            "a": np.random.normal(2, 1.2, size=250),
+            "b": np.random.normal(3, 1.5, size=250),
+            "c": np.random.choice(["x", "y"], size=250),
+        }
+    )
+    return df
+
+def test_heatmap(data):
+    with pytest.raises(OSError):
+        mw.data_heatmap(data)
 
 
-# def test_heatmap_static(data):
-#     fig = mw.data_heatmap(data, interactive=False)
-#     assert isinstance(fig, matplotlib.artist.Artist)
+def test_heatmap_static(data):
+    fig = mw.data_heatmap(data, interactive=False)
+    assert isinstance(fig, matplotlib.artist.Artist)
 
 
-# def test_heatmap_missing(data):
-#     fig = mw.data_heatmap(data, missing=True, interactive=False)
-#     assert isinstance(fig, matplotlib.artist.Artist)
+def test_heatmap_missing(data):
+    fig = mw.data_heatmap(data, missing=True, interactive=False)
+    assert isinstance(fig, matplotlib.artist.Artist)
 
 
-# def test_heatmap_numpy(data):
-#     data = data.select_dtypes(["number"]).to_numpy()
-#     fig = mw.data_heatmap(data, interactive=False)
-#     assert isinstance(fig, matplotlib.artist.Artist)
+def test_heatmap_numpy(data):
+    data = data.select_dtypes(["number"]).to_numpy()
+    fig = mw.data_heatmap(data, interactive=False)
+    assert isinstance(fig, matplotlib.artist.Artist)
 
 
-# def test_heatmap_invalid():
-#     data = [1, 2, 3, 4]
-#     with pytest.raises(ValueError):
-#         mw.data_heatmap(data)
+def test_heatmap_invalid():
+    data = [1, 2, 3, 4]
+    with pytest.raises(ValueError):
+        mw.data_heatmap(data)
