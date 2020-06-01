bfe65101c94a4c54749a9f63faa3a093027ea865
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-06-01 15:13:36 -0500

    Remove 'state' and add feature argument

--- mwdata/geospatial/mapping.py
+++ mwdata/geospatial/mapping.py
@@ -1,4 +1,5 @@
 import warnings
+
 import geoplot
 import numpy as np
 import geopandas as gpd
@@ -13,7 +14,7 @@ from mwdata.utilities.contextmanager import _context_manager
 def maps(
     data,
     map_type="choropleth",
-    color=None,
+    feature=None,
     choropleth_kwargs=None,
     kde_kwargs=None,
     context=None,
@@ -25,7 +26,8 @@ def maps(
         map_type: Default = 'choropleth'
             choropleth: Thematic map where areas are shaded in proportion to a statistical variable
             kde: Spatial density estimate plot for the distribution of input data
-        color: Color map by a numeric column
+        feature: If map_type='choropleth', color map by a numeric column. 
+        If map_type='kde', aggregate geometry based on feature
         choropleth_kwargs: Key word arguments for geopandas.plot
         kde_kwargs: Key word arguments for geoplot.kdeplot
         context: The context
@@ -40,13 +42,13 @@ def maps(
 
     if map_type == "choropleth":
         fig = choropleth(
-            data, color, choropleth_kwargs=choropleth_kwargs, context=context
+            data, feature, choropleth_kwargs=choropleth_kwargs, context=context
         )
 
     elif map_type == "kde":
         if data.shape[0] > 100:
             warnings.warn("Large number of rows, processing will take awhile")
-        fig = kde_map(data, kde_kwargs=kde_kwargs, context=context)
+        fig = kde_map(data, feature=feature, kde_kwargs=kde_kwargs, context=context)
 
     else:
         raise ValueError("{} map is not supported".format(map_type))
@@ -54,12 +56,12 @@ def maps(
 
 
 @_context_manager
-def choropleth(data, color=None, choropleth_kwargs=None, context=None):
+def choropleth(data, feature=None, choropleth_kwargs=None, context=None):
     """ Thematic map where areas are shaded in proportion to a statistical variable
 
     Args:
         data: Geopandas data frame
-        color: Color map by a numeric column
+        feature: Color map by a numeric column
         choropleth_kwargs: Key word arguments geopandas.plot
         context: The context
 
@@ -74,29 +76,30 @@ def choropleth(data, color=None, choropleth_kwargs=None, context=None):
     ax.set_ylabel("Latitude")
     ax.set_title("Choropleth Map")
 
-    if color is not None:
-        if np.issubdtype(data[color].dtype, np.number):
+    if feature is not None:
+        if np.issubdtype(data[feature].dtype, np.number):
             legend = True
             divider = make_axes_locatable(ax)
             cax = divider.append_axes("right", size="17%", pad=0.1)
-            cax.set_title(color)
+            cax.set_title(feature)
 
         else:
-            raise ValueError("{} should be numeric type".format(color))
+            raise ValueError("{} should be numeric type".format(feature))
     else:
         legend = False
         cax = None
 
-    fig = data.plot(column=color, legend=legend, ax=ax, cax=cax, **choropleth_kwargs)
+    fig = data.plot(column=feature, legend=legend, ax=ax, cax=cax, **choropleth_kwargs)
     return fig
 
 
 @_context_manager
-def kde_map(data, kde_kwargs=None, context=None):
+def kde_map(data, feature, kde_kwargs=None, context=None):
     """ Spatial density estimate plot for the distribution of input data
 
     Args:
         data: Geopandas data frame
+        feature: Feature to aggregate by
         kde_kwargs: Key word arguments for geoplot.kdeplot
         context: The context
 
@@ -118,7 +121,7 @@ def kde_map(data, kde_kwargs=None, context=None):
         ax = geoplot.kdeplot(
             df=data.geometry.centroid,
             figsize=(context.fig_width, context.fig_height),
-            clip=data.dissolve("state").geometry,
+            clip=data.dissolve(feature).geometry,
             shade_lowest=False,
             cmap="viridis",
             shade=True,
