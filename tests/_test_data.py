40d54aefe26a9c8a4d0206b8d53b11853c1b8fca
David Law <dvdlaw@gmail.com>
2020-05-31 21:43:32 -0500

    Fix distribution unit tests

--- tests/_test_data.py
+++ tests/_test_data.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
 
-
+np.random.seed(22)
 DATA = pd.DataFrame(
     {
         "a": np.random.normal(2, 1.2, size=250),
         "b": np.random.normal(3, 1.5, size=250),
         "c": np.random.normal(9, 0.2, size=250),
         "d": np.random.choice(["x", "y"], size=250),
+        "e": np.random.choice(["v", "w"], p=[0.01, 0.99], size=250),
     }
 )
