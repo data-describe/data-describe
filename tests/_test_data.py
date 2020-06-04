ac94c8db0ae83784061122e7956274a11fcaccac
Rishi Sheth <rishisheth@MWMac-RSHETH.local>
2020-06-03 22:21:17 -0500

    Fix pep8

--- tests/_test_data.py
+++ tests/_test_data.py
@@ -13,90 +13,105 @@ DATA = pd.DataFrame(
 )
 
 TEXT_DATA = {
-    'test_list_main':
-    [
+    "test_list_main": [
         "This is an article talking about Spider-man.",
         "Spiders are one of the weirdest things on earth.",
     ],
-
-    'answer_key_tokenized':
-    [
+    "answer_key_tokenized": [
         ["This", "is", "an", "article", "talking", "about", "Spider-man", "."],
-        ["Spiders", "are", "one", "of", "the", "weirdest", "things", "on", "earth", "."]
+        [
+            "Spiders",
+            "are",
+            "one",
+            "of",
+            "the",
+            "weirdest",
+            "things",
+            "on",
+            "earth",
+            ".",
+        ],
     ],
-
-    'answer_key_lower':
-    [
+    "answer_key_lower": [
         ["this", "is", "an", "article", "talking", "about", "spider-man", "."],
-        ["spiders", "are", "one", "of", "the", "weirdest", "things", "on", "earth", "."]
+        [
+            "spiders",
+            "are",
+            "one",
+            "of",
+            "the",
+            "weirdest",
+            "things",
+            "on",
+            "earth",
+            ".",
+        ],
     ],
-
-    'answer_key_remove_punct':
-    [
-        ['This', 'is', 'an', 'article', 'talking', 'about', 'Spider-man'],
-        ['Spiders', 'are', 'one', 'of', 'the', 'weirdest', 'things', 'on', 'earth']
+    "answer_key_remove_punct": [
+        ["This", "is", "an", "article", "talking", "about", "Spider-man"],
+        ["Spiders", "are", "one", "of", "the", "weirdest", "things", "on", "earth"],
     ],
-
-    'answer_key_remove_all_punct_no_space':
-    [
-        ['This', 'is', 'an', 'article', 'talking', 'about', 'Spiderman', ''],
-        ['Spiders', 'are', 'one', 'of', 'the', 'weirdest', 'things', 'on', 'earth', '']
+    "answer_key_remove_all_punct_no_space": [
+        ["This", "is", "an", "article", "talking", "about", "Spiderman", ""],
+        ["Spiders", "are", "one", "of", "the", "weirdest", "things", "on", "earth", ""],
     ],
-
-    'answer_key_remove_all_punct_with_space':
-    [
-        ['This', 'is', 'an', 'article', 'talking', 'about', 'Spider', 'man'],
-        ['Spiders', 'are', 'one', 'of', 'the', 'weirdest', 'things', 'on', 'earth']
+    "answer_key_remove_all_punct_with_space": [
+        ["This", "is", "an", "article", "talking", "about", "Spider", "man"],
+        ["Spiders", "are", "one", "of", "the", "weirdest", "things", "on", "earth"],
     ],
-
-    'answer_key_remove_stop_words':
-    [
-        ['article', 'talking', 'spider-man', '.'],
-        ['spiders', 'one', 'weirdest', 'things', 'earth', '.']
+    "answer_key_remove_stop_words": [
+        ["article", "talking", "spider-man", "."],
+        ["spiders", "one", "weirdest", "things", "earth", "."],
     ],
-
-    'answer_key_remove_stop_words_more':
-    [
-        ['article', 'talking', '.'],
-        ['one', 'weirdest', 'things', 'earth', '.']
+    "answer_key_remove_stop_words_more": [
+        ["article", "talking", "."],
+        ["one", "weirdest", "things", "earth", "."],
     ],
-
-    'test_list_digits':
-    [["th3is", "is", "8", "a", "d1s4ster", "but", "no", "numb3rs"]],
-
-    'answer_key_remove_digits':
-    [["", "is", "", "a", "", "but", "no", ""]],
-
-    'test_list_single_char_and_spaces':
-    [["a", " ", "nice", "    ", "day", "to", "b", "outside", "thank you"]],
-
-    'answer_key_single_char_and_spaces':
-    [["nice", "day", "to", "outside", "thank", "you"]],
-
-    'test_list_stopwords':
-    [["please", "do", "not", "eat", "the", "rest", "of", "my", "pineapple", "pizza"]],
-
-    'more_words':
-    ['spiders', 'spider-man'],
-
-    'answer_key_remove_stopwords':
-    [["please", "eat", "rest", "pineapple", "pizza"]],
-
-    'answer_key_remove_stopwords_more':
-    [["please", "rest", "pineapple"]],
-
-    'test_list_lem_and_stem':
-    ["Mars is the greatest planet to start terraforming; it would be amazing to see geese flying!"],
-
-    'answer_key_lem':
-    [["mar", "greatest", "planet", "start", "terraforming", "would", "amazing", "see", "goose", "flying"]],
-
-    'answer_key_stem':
-    [["mar", "greatest", "planet", "start", "terraform", "would", "amaz", "see", "gees", "fly"]],
-
-    'test_list_custom':
-    ["this is an absolutely phenomenal day."],
-
-    'answer_key_custom':
-    [["THIS", "IS", "AN", "ABSOLUTELY", "PHENOMENAL", "DAY", "."]]
+    "test_list_digits": [["th3is", "is", "8", "a", "d1s4ster", "but", "no", "numb3rs"]],
+    "answer_key_remove_digits": [["", "is", "", "a", "", "but", "no", ""]],
+    "test_list_single_char_and_spaces": [
+        ["a", " ", "nice", "    ", "day", "to", "b", "outside", "thank you"]
+    ],
+    "answer_key_single_char_and_spaces": [
+        ["nice", "day", "to", "outside", "thank", "you"]
+    ],
+    "test_list_stopwords": [
+        ["please", "do", "not", "eat", "the", "rest", "of", "my", "pineapple", "pizza"]
+    ],
+    "more_words": ["spiders", "spider-man"],
+    "answer_key_remove_stopwords": [["please", "eat", "rest", "pineapple", "pizza"]],
+    "answer_key_remove_stopwords_more": [["please", "rest", "pineapple"]],
+    "test_list_lem_and_stem": [
+        "Mars is the greatest planet to start terraforming; it would be amazing to see geese flying!"
+    ],
+    "answer_key_lem": [
+        [
+            "mar",
+            "greatest",
+            "planet",
+            "start",
+            "terraforming",
+            "would",
+            "amazing",
+            "see",
+            "goose",
+            "flying",
+        ]
+    ],
+    "answer_key_stem": [
+        [
+            "mar",
+            "greatest",
+            "planet",
+            "start",
+            "terraform",
+            "would",
+            "amaz",
+            "see",
+            "gees",
+            "fly",
+        ]
+    ],
+    "test_list_custom": ["this is an absolutely phenomenal day."],
+    "answer_key_custom": [["THIS", "IS", "AN", "ABSOLUTELY", "PHENOMENAL", "DAY", "."]],
 }
