845f8af3f7075a79cea13924328cb26c4ad027fc
Brian Ray <brian.ray@mavenwave.com>
2020-06-13 15:32:27 -0500

    remove Geo stuff for now fixes #94 until @soshel reviews

--- mwdata/utilities/load_data.py
+++ mwdata/utilities/load_data.py
@@ -3,10 +3,7 @@ import re
 import tempfile
 
 import pandas as pd
-import geopandas as gpd
 from google.cloud import storage
-
-GEO_EXTENSIONS = [".dbf", ".shp", ".shx"]
 
 
 def load_data(filepath, all_folders=False, **kwargs):
@@ -16,7 +13,7 @@ def load_data(filepath, all_folders=False, **kwargs):
         filepath: The file path. Can be either a local filepath or Google Cloud Storage URI filepath
         all_folders: If True, searches for text files in nested folders. If False, looks for text files in the current folder
         kwargs: Keyword arguments to pass to the reader
-            .shp: Uses geopandas.read_file
+
             .csv, .json, and other: Uses pandas.read_csv or pandas.read_json
 
     Returns:
@@ -58,7 +55,6 @@ def read_file_type(filepath, **kwargs):
     Args:
         filepath: The filepath to open
         kwargs: Keyword arguments to pass to the reader
-            .shp: Uses geopandas.read_file
             .csv, .json, and other: Uses pandas.read_csv or pandas.read_json
     Returns:
         A Pandas data frame
@@ -69,18 +65,6 @@ def read_file_type(filepath, **kwargs):
     elif extension == ".json":
         lines = kwargs.pop("lines", True)
         return pd.read_json(filepath, lines=lines, **kwargs)
-    elif extension == ".shp":
-        if "gs://" in filepath:
-            bucket = re.search(r"(?<=gs://)[\w-]*", filepath).group(0)
-            file = re.search(r"/[\w-]*.shp", filepath).group(0)
-            prefix = re.search(r"{0}.*/".format(bucket), filepath).group(0)
-            for ext in GEO_EXTENSIONS:
-                fpath = download_gcs_file(
-                    file + ext, bucket=bucket, prefix=prefix, **kwargs
-                )
-            return gpd.read_file(fpath, **kwargs)
-        else:
-            return gpd.read_file(filepath, **kwargs)
     elif extension == ".xlsx":
         return pd.read_excel(filepath, **kwargs)
     else:
