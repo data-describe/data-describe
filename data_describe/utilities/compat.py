a1b9cc7a31282b4210e3723873a5faf3f47f69a7
David Law <dvdlaw@gmail.com>
2020-06-21 19:47:24 -0500

    Rename package dict

--- data_describe/utilities/compat.py
+++ data_describe/utilities/compat.py
@@ -1,13 +1,13 @@
 from functools import wraps
 
-_DEPENDENCY_INSTALLED = {}
+_PACKAGE_INSTALLED = {}
 
 
 def requires(package_name):
     def f(func):
         @wraps(func)
         def g(*args, **kwargs):
-            if not _DEPENDENCY_INSTALLED[package_name]:
+            if not _PACKAGE_INSTALLED[package_name]:
                 raise ImportError(
                     f"Package {package_name} required to use this function"
                 )
@@ -21,7 +21,7 @@ def requires(package_name):
 try:
     import nltk  # noqa: F401
 
-    _DEPENDENCY_INSTALLED["nltk"] = True
+    _PACKAGE_INSTALLED["nltk"] = True
     try:
         nltk.data.find("tokenizers/punkt")
     except LookupError:
@@ -35,42 +35,42 @@ try:
     except LookupError:
         nltk.download("stopwords")
 except ImportError:
-    _DEPENDENCY_INSTALLED["nltk"] = False
+    _PACKAGE_INSTALLED["nltk"] = False
 
 try:
     import pyldavis  # noqa: F401
 
-    _DEPENDENCY_INSTALLED["pyldavis"] = True
+    _PACKAGE_INSTALLED["pyldavis"] = True
 except ImportError:
-    _DEPENDENCY_INSTALLED["pyldavis"] = False
+    _PACKAGE_INSTALLED["pyldavis"] = False
 
 try:
     import gensim  # noqa: F401
 
-    _DEPENDENCY_INSTALLED["gensim"] = True
+    _PACKAGE_INSTALLED["gensim"] = True
 except ImportError:
-    _DEPENDENCY_INSTALLED["gensim"] = False
+    _PACKAGE_INSTALLED["gensim"] = False
 
 try:
     import gcsfs  # noqa: F401
 
-    _DEPENDENCY_INSTALLED["gcsfs"] = True
+    _PACKAGE_INSTALLED["gcsfs"] = True
 except ImportError:
-    _DEPENDENCY_INSTALLED["gcsfs"] = False
+    _PACKAGE_INSTALLED["gcsfs"] = False
 
 try:
     import google.cloud.storage  # noqa: F401
 
-    _DEPENDENCY_INSTALLED["google-cloud-storage"] = True
+    _PACKAGE_INSTALLED["google-cloud-storage"] = True
 except ImportError:
-    _DEPENDENCY_INSTALLED["google-cloud-storage"] = False
+    _PACKAGE_INSTALLED["google-cloud-storage"] = False
 
 try:
     import modin.pandas
     import pandas
 
-    _DEPENDENCY_INSTALLED["modin"] = True
+    _PACKAGE_INSTALLED["modin"] = True
     _FRAME_TYPE = (pandas.DataFrame, modin.pandas.DataFrame)
 except ImportError:
-    _DEPENDENCY_INSTALLED["modin"] = False
+    _PACKAGE_INSTALLED["modin"] = False
     _FRAME_TYPE = pandas.DataFrame
