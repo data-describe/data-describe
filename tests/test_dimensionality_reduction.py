2c3b3e1a8c414e703ecd43405212e1b58d1e6325
Rishi Sheth <rishisheth@MWMac-RSHETH.local>
2020-06-22 19:27:44 -0500

    Initial adds to incorporate modin

--- tests/test_dimensionality_reduction.py
+++ tests/test_dimensionality_reduction.py
@@ -2,6 +2,7 @@ import pytest
 import sklearn
 import pandas as pd
 import numpy as np
+import modin.pandas as modin
 
 from data_describe.dimensionality_reduction.dimensionality_reduction import dim_reduc
 from ._test_data import DATA
@@ -10,6 +11,10 @@ from ._test_data import DATA
 @pytest.fixture
 def data():
     return DATA.select_dtypes(np.number)
+
+@pytest.fixture
+def modin_data():
+    return modin.DataFrame(DATA).select_dtypes(np.number)
 
 
 def test_error():
@@ -23,6 +28,11 @@ def test_pca(data):
     assert isinstance(x[0], pd.core.frame.DataFrame)
     assert isinstance(x[1], sklearn.decomposition.PCA)
 
+def test_pca_modin(modin_data):
+    x = dim_reduc(data=modin_data, n_components=2, dim_method="pca", df_type='modin')
+    assert isinstance(x, tuple)
+    assert isinstance(x[0], modin.dataframe.DataFrame)
+    assert isinstance(x[1], sklearn.decomposition.PCA)
 
 def test_tsne(data):
     x = dim_reduc(data=data, n_components=2, dim_method="tsne")
@@ -30,9 +40,20 @@ def test_tsne(data):
     assert isinstance(x[0], pd.core.frame.DataFrame)
     assert isinstance(x[1], sklearn.manifold.TSNE)
 
+def test_tsne_modin(modin_data):
+    x = dim_reduc(data=modin_data, n_components=2, dim_method="tsne",  df_type='modin')
+    assert isinstance(x, tuple)
+    assert isinstance(x[0], modin.dataframe.DataFrame)
+    assert isinstance(x[1], sklearn.manifold.TSNE)
 
 def test_tsvd(data):
     x = dim_reduc(data=data, n_components=2, dim_method="tsvd")
     assert isinstance(x, tuple)
     assert isinstance(x[0], pd.core.frame.DataFrame)
+    assert isinstance(x[1], sklearn.decomposition.TruncatedSVD)
+
+def test_tsvd_modin(modin_data):
+    x = dim_reduc(data=modin_data, n_components=2, dim_method="tsvd", df_type='modin')
+    assert isinstance(x, tuple)
+    assert isinstance(x[0], modin.dataframe.DataFrame)
     assert isinstance(x[1], sklearn.decomposition.TruncatedSVD)
