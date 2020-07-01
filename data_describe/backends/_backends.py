85f29b4e93772ee6bb0a81e13907a91d8dab8ef4
David Law <dvdlaw@gmail.com>
2020-06-30 23:26:18 -0500

    Add ordered check for compute plugins

--- data_describe/backends/_backends.py
+++ data_describe/backends/_backends.py
@@ -1,6 +1,6 @@
 import importlib
 from types import ModuleType
-from typing import Dict
+from typing import Dict, List
 
 from data_describe.config._config import get_option
 from data_describe.compat import _DATAFRAME_BACKENDS
@@ -10,25 +10,27 @@ _viz_backends: Dict[str, ModuleType] = {}
 _compute_backends: Dict[str, ModuleType] = {}
 
 
+class Backend:
+    def __init__(self, b: List[ModuleType]):
+        """List of modules to search for implementation"""
+        self.b = b
+
+    def __getattr__(self, f):
+        for module in self.b:
+            try:
+                return module.__getattribute__(f)
+            except AttributeError:
+                pass
+        raise ModuleNotFoundError(f"Could not find implementation for {f}")
+
+
 def _get_viz_backend(backend=None):
     backend = backend or get_option("backends.viz")
 
-    if backend == "matplotlib":
-        try:
-            import data_describe.backends.viz._matplotlib  # noqa
-        except ImportError:
-            raise ImportError(
-                "matplotlib is required for plotting when the "
-                "default backend 'matplotlib' is selected."
-            ) from None
-
-    # Backend already loaded
-    if backend in _viz_backends:
-        return _viz_backends[backend]
-
-    module = _find_viz_backend(backend)
-    _viz_backends[backend] = module
-    return module
+    if backend not in _viz_backends:
+        module = _find_viz_backend(backend)
+        _viz_backends[backend] = module
+    return Backend([_viz_backends[backend]])
 
 
 def _find_viz_backend(backend=None):
@@ -58,27 +60,23 @@ def _find_viz_backend(backend=None):
 
 
 def _get_compute_backend(backend=None, df=None):
-    backend = (
-        backend
-        or get_option("backends.compute")
-        or _DATAFRAME_BACKENDS.get(str(type(df)), None)
-    )
+    data_type = str(type(df))
+    backend_sources = [
+        backend,
+        _DATAFRAME_BACKENDS.get(data_type, None),
+        get_option("backends.compute"),
+    ]
 
-    if backend == "pandas":
-        try:
-            import data_describe.backends.compute._pandas  # noqa
-        except ImportError:
-            raise ImportError(
-                "pandas is required when the default backend 'pandas' is selected."
-            ) from None
-
-    # Backend already loaded
-    if backend in _compute_backends:
-        return _compute_backends[backend]
-
-    module = _find_compute_backend(backend)
-    _compute_backends[backend] = module
-    return module
+    backend_list = []
+    for backend in backend_sources:
+        if backend:
+            if backend not in _compute_backends:
+                module = _find_compute_backend(backend)
+                _compute_backends[backend] = module
+            else:
+                module = _compute_backends[backend]
+            backend_list.append(module)
+    return Backend(backend_list)
 
 
 def _find_compute_backend(backend=None):
