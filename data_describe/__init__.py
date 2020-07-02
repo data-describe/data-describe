eeea64a509a7d7035d14fc5dac4d3b180e5a1eba
Rishi Sheth <rishisheth@MWMac-RSHETH.local>
2020-07-02 09:58:41 -0500

    Import config in init file

--- data_describe/__init__.py
+++ data_describe/__init__.py
@@ -9,6 +9,7 @@ from data_describe.core.scatter_plot import scatter_plots  # noqa: F401
 from data_describe.core.correlation_matrix import correlation_matrix  # noqa: F401
 from data_describe.core.importance import importance  # noqa: F401
 from data_describe.core.cluster import cluster  # noqa: F401
+from data_describe.config._config import options
 
 
 # Check for plotly extensions
