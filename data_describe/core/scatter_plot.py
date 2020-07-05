67d1e231392d0cc3488ec61af8c9eb60fa5986e5
David Law <dvdlaw@gmail.com>
2020-07-04 19:33:45 -0500

    Use new pattern for scatter plot feature

--- data_describe/core/scatter_plot.py
+++ data_describe/core/scatter_plot.py
@@ -1,147 +1,37 @@
-import warnings
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pyscagnostics import scagnostics
+from data_describe.backends import _get_compute_backend, _get_viz_backend
 
 
 def scatter_plots(
     data,
-    plot_mode="matrix",
+    mode="matrix",
+    sample=None,
     threshold=None,
-    joint_kws=None,
-    scatter_kws=None,
-    dist_kws=None,
+    compute_backend=None,
+    viz_backend=None,
+    **kwargs
 ):
     """Scatter plots
 
     Args:
-        data:  A Pandas data frame
-        plot_mode: Default = 'diagnostic"
-            diagnostic: Choose plots by scatter plot diagnostics
+        data: A Pandas data frame
+        mode: The visualization mode
+            diagnostic: Plots selected by scagnostics (scatter plot diagnostics)
             matrix: Generate the full scatter plot matrix
             all: Generate all individual scatter plots
-        threshold: The scatter plot diagnostic threshold value [0,1] for returning a plot
+        sample: The sampling method to use
+        threshold: The scatter plot diagnostic threshold value [0,1] for returning a plot. Only used with "diagnostic" mode.
             If a number: Returns all plots where at least one metric is above this threshold
-            If a dictionary: Returns plots where the metric is above its threshold
-            For example, {"Outlier": 0.9} returns plots with outlier metrics above 0.9
-            The available metrics are: Outlier, Convex, Skinny, Skewed, Stringy, Straight, Monotonic, Clumpy, Striated
-        joint_kws: Keywords to pass to seaborn.JointGrid
-        scatter_kws: Keywords to pass to seaborn.scatterplot
-        dist_kws: Keywords to pass to the seaborn.distplot
-
-    Returns:
-        Seaborn figure or list of figures
+            If a dictionary: Returns plots where the metric is above its threshold.
+            For example, {"Outlying": 0.9} returns plots with outlier metrics above 0.9.
+            See pyscagnostics.measure_names for a list of metrics.
+        compute_backend: The compute backend
+        viz_backend: The vizualization backend
+        kwargs: Passed to the visualization framework
     """
-    if isinstance(data, pd.DataFrame):
-        data = data.select_dtypes(["number"])
-    else:
-        raise NotImplementedError("Must be a Pandas data frame")
+    data = _get_compute_backend(compute_backend).process_scatter_plot(
+        data, mode, sample, threshold, **kwargs
+    )
 
-    if plot_mode == "matrix":
-        fig = sns.pairplot(data)
-        plt.show()
-        plt.close()
-        return fig
-    elif plot_mode == "all":
-        num_df = data.select_dtypes(["number"])
-        pairs = [
-            (i, j)
-            for i in range(len(num_df.columns))
-            for j in range(len(num_df.columns))
-            if j > i
-        ]
-        fig = []
-        with warnings.catch_warnings():
-            warnings.filterwarnings(
-                "ignore",
-                category=FutureWarning,
-                message=r"Using a non-tuple sequence for multidimensional indexing is deprecated",
-            )
-            for p in pairs:
-                x_col = num_df.columns.values[p[0]]
-                y_col = num_df.columns.values[p[1]]
-                fig.append(
-                    scatter_plot(data, x_col, y_col, joint_kws, scatter_kws, dist_kws)
-                )
-            return fig
-
-    elif plot_mode == "diagnostic":
-        diagnostics = scagnostics(data)
-
-        if threshold is not None:
-            diagnostics = filter_threshold(diagnostics, threshold)
-
-        if len(diagnostics) == 0:
-            raise UserWarning("No plots identified by diagnostics")
-
-        fig = []
-        for d in diagnostics:
-            fig.append(
-                scatter_plot(
-                    data, d[0], d[1], joint_kws, scatter_kws, dist_kws, context,
-                )
-            )
-
-        return fig
-    else:
-        raise ValueError(f"Unknown plot mode: {plot_mode}")
-
-
-def scatter_plot(data, x, y, joint_kws=None, scatter_kws=None, dist_kws=None):
-    """Generate one scatter (joint) plot
-
-    Args:
-        data: A Pandas data frame
-        x: The x-axis column name
-        y: The y-axis column name
-        joint_kws: Keywords to pass to seaborn.JointGrid
-        scatter_kws: Keywords to pass to seaborn.scatterplot
-        dist_kws: Keywords to pass to the seaborn.distplot
-
-
-    Returns:
-        The Seaborn figure
-    """
-    if joint_kws is None:
-        joint_kws = {}
-        # joint_kws = {"height": max(context.fig_width.fig_height)} # TODO (haishiro): Replace with get_option
-        pass
-    if scatter_kws is None:
-        scatter_kws = {}
-    if dist_kws is None:
-        dist_kws = {"kde": False, "rug": False}
-
-    g = sns.JointGrid(data[x], data[y], **joint_kws)
-    g = g.plot_joint(sns.scatterplot, **scatter_kws)
-    g = g.plot_marginals(sns.distplot, **dist_kws)
-    plt.show()
-    plt.close()
-    return g
-
-
-def filter_threshold(diagnostics, threshold=0.85):
-    """Filter the plots by scatter plot diagnostic threshold
-
-    Args:
-        diagnostics: The diagnostics generator from pyscagnostics
-        threshold: The scatter plot diagnostic threshold value [0,1] for returning a plot
-            If a number: Returns all plots where at least one metric is above this threshold
-            If a dictionary: Returns plots where the metric is above its threshold
-            For example, {"Outlier": 0.9} returns plots with outlier metrics above 0.9
-            The available metrics are: Outlier, Convex, Skinny, Skewed, Stringy, Straight, Monotonic, Clumpy, Striated
-
-    Returns:
-        A dictionary of pairs that match the filter
-    """
-    if isinstance(threshold, dict):
-        return [
-            d
-            for d in diagnostics
-            if all([d[2][0][m] >= threshold[m] for m in threshold.keys()])
-        ]
-    else:
-        return [
-            d for d in diagnostics if any([v > threshold for v in d[2][0].values()])
-        ]
+    return _get_viz_backend(viz_backend).viz_scatter_plot(
+        data, mode, sample, threshold, **kwargs
+    )
--- tests/test_scatter_plot.py
+++ tests/test_scatter_plot.py
@@ -9,29 +9,26 @@ matplotlib.use("Agg")
 
 def test_scatter_plot_matrix(data):
     data = data.dropna(axis=1, how="all")
-    fig = dd.scatter_plots(data, plot_mode="matrix")
+    fig = dd.scatter_plots(data, mode="matrix")
     assert isinstance(fig, seaborn.axisgrid.PairGrid)
 
 
 def test_scatter_plot_all(data):
     data = data.dropna(axis=1, how="all")
-    fig = dd.scatter_plots(data, plot_mode="all")
+    fig = dd.scatter_plots(data, mode="all")
     assert isinstance(fig, list)
     assert isinstance(fig[0], seaborn.axisgrid.JointGrid)
 
 
 def test_scatter_plot(data):
-    fig = dd.scatter_plots(data, plot_mode="diagnostic", threshold=0.15)
+    fig = dd.scatter_plots(data, mode="diagnostic", threshold=0.15)
     assert isinstance(fig, list)
     assert isinstance(fig[0], seaborn.axisgrid.JointGrid)
 
 
 def test_scatter_plot_dict(data):
     fig = dd.scatter_plots(
-        data,
-        plot_mode="diagnostic",
-        threshold={"Outlying": 0.1},
-        dist_kws={"rug": True},
+        data, mode="diagnostic", threshold={"Outlying": 0.1}, dist_kws={"rug": True}
     )
     assert isinstance(fig, list)
     assert isinstance(fig[0], seaborn.axisgrid.JointGrid)
@@ -41,12 +38,12 @@ def test_scatter_plot_outside_threshold(data):
     with pytest.raises(UserWarning, match="No plots identified by diagnostics"):
         dd.scatter_plots(
             data,
-            plot_mode="diagnostic",
+            mode="diagnostic",
             threshold={"Outlying": 0.999},
             dist_kws={"rug": True},
         )
 
 
 def test_scatter_plot_wrong_data_type(data):
-    with pytest.raises(NotImplementedError):
+    with pytest.raises(ValueError):
         dd.scatter_plots([1, 2, 3])
