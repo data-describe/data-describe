67b96054025e72cbb7f088c3c6428b4fd90c354e
Rishi Sheth <rishisheth@MWMac-RSHETH.local>
2020-07-08 14:29:56 -0500

    Remove unused import _modin file from compute.__init__ file

--- data_describe/backends/compute/__init__.py
+++ data_describe/backends/compute/__init__.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-import data_describe.backends.compute._pandas # noqa
-import data_describe.backends.compute._modin # noqa
-
-=======
 import data_describe.backends.compute._pandas  # noqa
 import data_describe.backends.compute._modin
->>>>>>> Edit docstrings and pep8 fixes
