945ab37b1da4ba4ab60b8c97fbae8029bd73c213
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-13 17:18:24 -0500

    Add function to __init__.py

--- data_describe/backends/viz/_seaborn/__init__.py
+++ data_describe/backends/viz/_seaborn/__init__.py
@@ -3,4 +3,5 @@ from data_describe.backends.viz._seaborn.data_heatmap import (  # noqa: F401
 )
 from data_describe.backends.viz._seaborn.time_series import (  # noqa: F401
     viz_plot_time_series,
+    viz_plot_autocorrelation,
 )
