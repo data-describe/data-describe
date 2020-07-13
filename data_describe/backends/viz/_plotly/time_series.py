43462fff270c21194474562aa5cb68683113ab43
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-13 17:17:49 -0500

    Add autocorrelation and clean script

--- data_describe/backends/viz/_plotly/time_series.py
+++ data_describe/backends/viz/_plotly/time_series.py
@@ -1,44 +1,37 @@
 import plotly.graph_objs as go
-from statsmodels.tsa.stattools import acf, pacf
+from plotly.subplots import make_subplots
+from plotly.offline import init_notebook_mode, iplot
+from IPython import get_ipython
 
 
-# def viz_time_series(data):
-
-
-def plotly_time_series(df, cols=None):
-    data = []
+def viz_plot_time_series(df=None, cols=None, result=None, decompose=False):
     if isinstance(cols, list):
-        for i in cols:
-            data.append(go.Scatter(x=df.index, y=df[i]))
+        data = [go.Scatter(x=df.index, y=df[c]) for c in cols]
+        fig = go.Figure(data=data)
     elif isinstance(cols, str):
-        data.append(go.Scatter(x=df.index, y=df[cols]))
-    return go.Figure(data=data)
-
-
-def plotly_pacf(df, n_lags):
-    data = [go.Bar(y=pacf(df, n_lags))]
-    fig = go.figure(data=data)
-    # use iplot to visualize
-    return fig
-
-
-def plotly_acf(df, n_lags=40):
-    data = [go.Bar(y=acf(df, n_lags))]
-    fig = go.figure(data=data)
-    # use iplot to visualize
-    return fig
-
-
-def plotly_decomposition(result, plot_type="trend"):
-    if plot_type == "trend":
-        data = [go.Scatter(x=result.trend.index, y=result.trend)]
-    elif plot_type == "residual":
-        data = [go.Scatter(x=result.resid.index, y=result.resid)]
-    elif plot_type == "seasonal":
-        data = [go.Scatter(x=result.seasonal.index, y=result.seasonal)]
+        fig = go.Figure(data=go.Scatter(x=df, y=df[cols]))
+    elif decompose:
+        fig = viz_decomposition(result)  # need to pass into figure
+    if get_ipython() is not None:  # remove none?
+        init_notebook_mode(connected=True)
+        return iplot(fig, config={"displayModeBar": False})
     else:
-        raise ValueError(
-            "Please choose between trend, residual, and seasonal for plot_types"
-        )
-    fig = go.figure(data=data)
+        return fig
+
+
+def viz_decomposition(result):
+    decompose_results = [result.observed, result.trend, result.seasonal, result.resid]
+    fig = make_subplots(rows=4, cols=1)
+    for idx, timeseries in enumerate(decompose_results, 1):
+        fig.add_trace(go.Scatter(y=timeseries), row=idx, col=1)
     return fig
+
+
+def viz_plot_autocorrelation(data, plot_type="acf"):
+    if plot_type == "acf":
+        data = [go.Bar(y=data)]
+    elif plot_type == "pacf":
+        data = [go.Bar(y=data)]
+    else:
+        raise ValueError("Unsupported input data type")
+    return go.figure(data=data)
