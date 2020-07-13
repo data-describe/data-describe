bea338a44de8b90a09df80640c9d85d838974b54
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-13 17:09:48 -0500

    Add functions to __init__.py

--- data_describe/backends/viz/_seaborn/__init__.py
+++ data_describe/backends/viz/_seaborn/__init__.py
@@ -1,7 +1,10 @@
 from data_describe.backends.viz._seaborn.data_heatmap import (  # noqa: F401
-    viz_data_heatmap
+    viz_data_heatmap,
 )
 from data_describe.backends.viz._seaborn.scatter_plot import (  # noqa: F401
-    viz_scatter_plot
+    viz_scatter_plot,
 )
 from data_describe.backends.viz._seaborn.importance import viz_importance  # noqa: F401
+from data_describe.backends.viz._seaborn.time_series import (  # noqa: F401
+    viz_plot_time_series,
+)
