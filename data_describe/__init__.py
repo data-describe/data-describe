dcd43da546c1d926fc7d290a0246f5ec98f5df47
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-14 16:39:39 -0500

    Update __init__.py

--- data_describe/__init__.py
+++ data_describe/__init__.py
@@ -9,6 +9,7 @@ from data_describe.core.scatter_plot import scatter_plots  # noqa: F401
 from data_describe.core.correlation_matrix import correlation_matrix  # noqa: F401
 from data_describe.core.importance import importance  # noqa: F401
 from data_describe.core.cluster import cluster  # noqa: F401
+from data_describe.core.time_series import plot_time_series  # noqa: F401
 from data_describe.config._config import options  # noqa: F401
 
 # Check for plotly extensions
