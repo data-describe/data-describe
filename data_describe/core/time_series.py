0fa2b7b53d4efa6e951eb4acf2528a81efe63d8f
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-15 12:42:45 -0500

    Add to function signature and add logic

--- data_describe/core/time_series.py
+++ data_describe/core/time_series.py
@@ -33,3 +33,20 @@ def stationarity_test(data, test="dickey-fuller", compute_backend=None, **kwargs
         data, test, **kwargs
     )
     return data
+
+
+def plot_autocorrelation(
+    df, plot_type="acf", n_lags=40, compute_backend=None, viz_backend=None, **kwargs
+):
+    if viz_backend == "plotly":
+        data = _get_compute_backend(compute_backend, df).compute_autocorrelation(
+            df, plot_type=plot_type, n_lags=n_lags, **kwargs
+        )
+        fig = _get_viz_backend(viz_backend).viz_plot_autocorrelation(
+            data, plot_type=plot_type, **kwargs
+        )
+    else:
+        fig = _get_viz_backend(viz_backend).viz_plot_autocorrelation(
+            df, plot_type=plot_type, n_lags=n_lags, **kwargs
+        )
+    return fig
