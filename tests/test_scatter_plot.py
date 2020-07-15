7ea403cf2d89f7293b46ba0e392c8b8d92fcdc1d
David Law <dvdlaw@gmail.com>
2020-07-15 08:49:16 -0500

    Add extension checker (Fixes #22) (#134)
    * Add file extension changes
    
    * Make file type enum private
    
    * Fix unit test to be os independent
    
    * Fix nltk find (#155)
    
    * Fix issue link (#153)
    
    * Rename config_context to update_context (#154)
    
    * Add file extension changes
    
    * Make file type enum private
    
    * Fix unit test to be os independent
    
    * Fix import alias in tests

--- tests/test_scatter_plot.py
+++ tests/test_scatter_plot.py
@@ -4,18 +4,21 @@ import pytest
 
 import data_describe as dd
 
+
 matplotlib.use("Agg")
 
 
 def test_scatter_plot_matrix(data):
     data = data.dropna(axis=1, how="all")
     fig = dd.scatter_plots(data, mode="matrix")
+
     assert isinstance(fig, seaborn.axisgrid.PairGrid)
 
 
 def test_scatter_plot_all(data):
     data = data.dropna(axis=1, how="all")
     fig = dd.scatter_plots(data, mode="all")
+
     assert isinstance(fig, list)
     assert isinstance(fig[0], seaborn.axisgrid.JointGrid)
 
@@ -36,6 +39,7 @@ def test_scatter_plot_dict(data):
 
 def test_scatter_plot_outside_threshold(data):
     with pytest.raises(UserWarning, match="No plots identified by diagnostics"):
+
         dd.scatter_plots(
             data,
             mode="diagnostic",
