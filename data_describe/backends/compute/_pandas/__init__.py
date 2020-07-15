c0dce8211624838274fa350d020a94aba039ce05
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-15 12:43:18 -0500

    Add function to init

--- data_describe/backends/compute/_pandas/__init__.py
+++ data_describe/backends/compute/_pandas/__init__.py
@@ -4,4 +4,5 @@ from data_describe.backends.compute._pandas.data_heatmap import (  # noqa: F401
 from data_describe.backends.compute._pandas.time_series import (  # noqa: F401
     compute_stationarity_test,
     compute_decompose_timeseries,
+    compute_autocorrelation,
 )
