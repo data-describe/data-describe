e4a0356e8503899ca13ef2421a1770de5b5b155f
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-16 16:05:09 -0500

    Adjust parameters and catch warning

--- data_describe/backends/compute/_pandas/time_series.py
+++ data_describe/backends/compute/_pandas/time_series.py
@@ -1,46 +1,46 @@
+import warnings
+
 from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.tsa.stattools import acf, pacf
-from statsmodels.tsa.stattools import adfuller, kpss
+from statsmodels.tsa.stattools import acf, pacf, adfuller, kpss
 import pandas as pd
 
-from data_describe.compat import _DATAFRAME_TYPE
 
-
-def compute_stationarity_test(df, col, test="dickey-fuller"):
+def compute_stationarity_test(
+    timeseries, test="dickey-fuller", regression="c", **kwargs
+):
     """Perform stationarity tests to see if mean and variance are changing over time.
     Backend uses statsmodel's  statsmodels.tsa.stattools.adfuller or statsmodels.tsa.stattools.kpss
 
     Args:
-        df: Dataframe containing a datetime index
-        col: The feature of interest
+        timeseries: Series containing a datetime index
+        regression: Constant and trend order to include in regression. Choose between 'c','ct','ctt', and 'nc'
         test: Choice of stationarity test. "kpss" or "dickey-fuller". Defaults to "dickey-fuller".
 
     Returns:
         st: Pandas dataframe containing the statistics
     """
-    if test == "dickey-fuller":
-        st = adf_test(df[col])
-    elif test == "kpss":
-        st = kpss_test(df[col])
+    if test.lower() == "dickey-fuller":
+        st = adf_test(timeseries, regression=regression, **kwargs)
+    elif test.lower() == "kpss":
+        st = kpss_test(timeseries, regression=regression, **kwargs)
     else:
         raise ValueError(f"{test} not implemented")
     return st
 
 
-def adf_test(df, autolag="AIC", regression="c"):
+def adf_test(timeseries, autolag="AIC", regression="c"):
     """Compute the Augmented Dickey-Fuller (ADF) test for stationarity
     Backend uses statsmodels.tsa.stattools.adfuller
 
-
     Args:
-        df: The dataframe
+        timeseries: The timeseries
         autolag: Method to use when determining the number of lags. Defaults to 'AIC'. Choose between 'AIC', 'BIC', 't-stat', and None
         regression: Constant and trend order to include in regression. Choose between 'c','ct','ctt', and 'nc'
 
     Returns:
         Pandas dataframe containing the statistics
     """
-    test = adfuller(df, autolag=autolag, regression=regression)
+    test = adfuller(timeseries, autolag=autolag, regression=regression)
     adf_output = pd.Series(
         test[0:4],
         index=[
@@ -55,12 +55,12 @@ def adf_test(df, autolag="AIC", regression="c"):
     return pd.DataFrame(adf_output, columns=["stats"])
 
 
-def kpss_test(df, regression="c", nlags=None):
+def kpss_test(timeseries, regression="c", nlags=None):
     """Compute the Kwiatkowski–Phillips–Schmidt–Shin (KPSS) test for stationarity.
     Backend uses statsmodels.tsa.stattools.kpss
 
     Args:
-        df ([type]): [description]
+        timeseries: The timeseries
         regression: The null hypothesis for the KPSS test.
             'c' : The data is stationary around a constant (default).
             'ct' : The data is stationary around a trend.
@@ -69,7 +69,13 @@ def kpss_test(df, regression="c", nlags=None):
     Returns:
         Pandas dataframe containing the statistics
     """
-    test = kpss(df, regression="c")
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore",
+            category=FutureWarning,
+            message="The behavior of using lags=None will change in the next release.",
+        )
+        test = kpss(timeseries, regression="c")
     kpss_output = pd.Series(test[0:3], index=["Test Statistic", "p-value", "Lags Used"])
     for key, value in test[3].items():
         kpss_output["Critical Value (%s)" % key] = value
@@ -81,17 +87,13 @@ def compute_decompose_timeseries(df, col, model="additive"):
 
     Args:
         df: The dataframe
-        col: The col of interest. Must be numeric
+        col: The col of interest. Must be numeric datatype
         model: Type of seasonal component. Defaults to "additive".
 
     Returns:
         result: statsmodels.tsa.seasonal.DecomposeResult object
     """
-    if isinstance(df, _DATAFRAME_TYPE):
-        result = seasonal_decompose(df[col], model=model)
-    else:
-        raise ValueError("Unsupported input data type")
-    return result
+    return seasonal_decompose(df[col], model=model)
 
 
 # check kwargs are passed
