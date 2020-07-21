bffed6da5d45aad18b4a5c24d21a10b36b4ea457
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-21 14:56:43 -0500

    Add typing checks and fix format'

--- data_describe/backends/viz/__init__.py
+++ data_describe/backends/viz/__init__.py
@@ -2,4 +2,9 @@ import data_describe.backends.viz._seaborn  # noqa
 import data_describe.backends.viz._plotly  # noqa
 
 
-__all__ = ["viz_data_heatmap", "viz_importance", "viz_plot_time_series", "viz_plot_autocorrelation]
+__all__ = [
+    "viz_data_heatmap",
+    "viz_importance",
+    "viz_plot_time_series",
+    "viz_plot_autocorrelation",
+]
