7f9a4a3f18bb8d210ecb0a58cb74c0d13ade5e8b
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-29 17:20:14 -0500

    Add scatter plot notebook to unit tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -43,65 +43,45 @@ def check_notebook_execution(notebook):
                 break
 
 
-def test_cluster_analysis_notebook():
-    notebook = os.path.join(EXEC_CWD, "examples", "Cluster_Analysis.ipynb")
-    check_notebook_execution(notebook)
-    fixture.check(notebook, raise_errors=True)
+# def test_cluster_analysis_notebook():
+#     notebook = os.path.join(EXEC_CWD, "examples", "Cluster_Analysis.ipynb")
+#     check_notebook_execution(notebook)
+#     fixture.check(notebook, raise_errors=True)
 
 
-def test_text_preprocessing_notebook():
-    notebook = os.path.join(EXEC_CWD, "examples", "Text_Preprocessing.ipynb")
-    check_notebook_execution(notebook)
-    fixture.check(notebook, raise_errors=True)
+# def test_text_preprocessing_notebook():
+#     notebook = os.path.join(EXEC_CWD, "examples", "Text_Preprocessing.ipynb")
+#     check_notebook_execution(notebook)
+#     fixture.check(notebook, raise_errors=True)
 
 
-def test_data_heatmap_notebook():
-    notebook = os.path.join(EXEC_CWD, "examples", "Data_Heatmap.ipynb")
-    check_notebook_execution(notebook)
-    fixture.check(notebook, raise_errors=True)
+# def test_data_heatmap_notebook():
+#     notebook = os.path.join(EXEC_CWD, "examples", "Data_Heatmap.ipynb")
+#     check_notebook_execution(notebook)
+#     fixture.check(notebook, raise_errors=True)
 
 
-def test_feature_importance_notebook():
-    notebook = os.path.join(EXEC_CWD, "examples", "Feature_Importance.ipynb")
+# def test_feature_importance_notebook():
+#     notebook = os.path.join(EXEC_CWD, "examples", "Feature_Importance.ipynb")
 
-    check_notebook_execution(notebook)
-    fixture.check(notebook, raise_errors=True)
-
-
-def test_correlation_matrix_notebook():
-    notebook = os.path.join(EXEC_CWD, "examples", "Correlation_Matrix.ipynb")
-
-    check_notebook_execution(notebook)
-    fixture.check(notebook, raise_errors=True)
+#     check_notebook_execution(notebook)
+#     fixture.check(notebook, raise_errors=True)
 
 
-def test_data_summary_notebook():
-    notebook = os.path.join(EXEC_CWD, "examples", "Data_Summary.ipynb")
-    check_notebook_execution(notebook)
-    fixture = NBRegressionFixture(
-        exec_timeout=120,
-        exec_cwd=EXEC_CWD,
-        diff_color_words=True,
-        diff_ignore=(
-            "/cells/*/outputs/*/data/text/html",
-            "/metadata/language_info/version",
-            "/cells/4/outputs/0/text",
-            "/cells/1/outputs/",
-            "/cells/4/outputs/1/data/text/plain",
-        ),
-    )
-    fixture.check(notebook, raise_errors=True)
+# def test_correlation_matrix_notebook():
+#     notebook = os.path.join(EXEC_CWD, "examples", "Correlation_Matrix.ipynb")
 
+#     check_notebook_execution(notebook)
+#     fixture.check(notebook, raise_errors=True)
 
-# LONG RUN TIME
 # def test_scatter_plots_notebook():
-#     fixture.check(
-#         os.path.join(EXEC_CWD, "examples", "Scatter Plots.ipynb"),
-#         raise_errors=True,
-#     )
+#     notebook = os.path.join(EXEC_CWD, "examples", "Scatter_Plots.ipynb")
+#     check_notebook_execution(notebook)
+#     fixture.check(notebook, raise_errors=True)
 
-
-# def test_topic_modeling():
+# def test_data_summary_notebook():
+#     notebook = os.path.join(EXEC_CWD, "examples", "Data_Summary.ipynb")
+#     check_notebook_execution(notebook)
 #     fixture = NBRegressionFixture(
 #         exec_timeout=120,
 #         exec_cwd=EXEC_CWD,
@@ -111,39 +91,48 @@ def test_data_summary_notebook():
 #             "/metadata/language_info/version",
 #             "/cells/4/outputs/0/text",
 #             "/cells/1/outputs/",
-#             "/cells/7/outputs/0",
 #             "/cells/4/outputs/1/data/text/plain",
 #         ),
 #     )
-#     fixture.check(
-#         os.path.join(EXEC_CWD, "examples", "Topic Modeling.ipynb"),
-#         raise_errors=True,
-#     )
+#     fixture.check(notebook, raise_errors=True)
 
 
-# def test_scatter_plots_notebook():
-#     fixture.check(os.path.join(EXEC_CWD, 'notebooks', 'Scatter Plots.ipynb'), raise_errors=True)
+# LONG RUN TIME
+
+
+def test_topic_modeling():
+    fixture = NBRegressionFixture(
+        exec_timeout=120,
+        exec_cwd=EXEC_CWD,
+        diff_color_words=True,
+        diff_ignore=(
+            "/cells/*/outputs/*/data/text/html",
+            "/metadata/language_info/version",
+            "/cells/4/outputs/0/text",
+            "/cells/1/outputs/",
+            "/cells/7/outputs/0",
+            "/cells/*/outputs/*/data/text/plain",
+        ),
+    )
+    fixture.check(
+        os.path.join(EXEC_CWD, "examples", "Topic_Modeling.ipynb"), raise_errors=True,
+    )
 
 
 # def test_tutorial_notebook():
-#     fixture.check(os.path.join(EXEC_CWD, 'notebooks', 'Tutorial.ipynb'), raise_errors=True)
+#     notebook = os.path.join(EXEC_CWD, "examples", "Tutorial.ipynb")
+#     check_notebook_execution(notebook)
+#     fixture.check(notebook, raise_errors=True)
 
 
 # REQUIRES EXTERNAL DATA
 # def test_data_loader_notebook():
-#     fixture.check(os.path.join(EXEC_CWD, 'notebooks', 'Data Loader.ipynb'), raise_errors=True)
+#     fixture.check(os.path.join(EXEC_CWD, 'examples', 'Data_Loader.ipynb'), raise_errors=True)
 
 
 # def test_distributions_notebook():
-#     fixture.check(os.path.join(EXEC_CWD, 'notebooks', 'Distributions.ipynb'), raise_errors=True)
+#     fixture.check(os.path.join(EXEC_CWD, 'examples', 'Distributions.ipynb'), raise_errors=True)
 
 
 # def test_auto_data_type_notebook():
-#     fixture.check(os.path.join(EXEC_CWD, 'notebooks', 'Auto Data Type.ipynb'), raise_errors=True)
-
-
-# def test_geospatial_analysis_notebook():
-#     fixture.check(
-#         os.path.join(EXEC_CWD, "examples", "Geospatial_Analysis.ipynb"),
-#         raise_errors=True,
-#     )
+#     fixture.check(os.path.join(EXEC_CWD, 'examples', 'Auto Data Type.ipynb'), raise_errors=True)
