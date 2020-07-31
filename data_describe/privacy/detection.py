8ff1144ac4ac8c218fcf89b96f05405721bcf91d
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-07-30 21:03:29 -0500

    Add back presidio initialization to engine.py

--- data_describe/privacy/detection.py
+++ data_describe/privacy/detection.py
@@ -1,61 +1,8 @@
 from typing import Optional
-import contextlib
-import logging
 
 from data_describe.backends import _get_compute_backend
 from data_describe.compat import _DATAFRAME_TYPE
-from data_describe.config._config import set_config, get_option
-from data_describe.compat import presidio_analyzer
-
-# from data_describe.config._config import set_config, get_option
-
-# from data_describe.privacy.engine import engine
-# import contextlib
-# import logging
-
-# from data_describe.compat import presidio_analyzer
-# from data_describe.config._config import set_config, get_option
-
-
-set_config({"sensitive_data.sample_size": 100, "sensitive_data.score_threshold": 0.2})
-_DEFAULT_SCORE_THRESHOLD = get_option("sensitive_data.score_threshold")
-
-logger = logging.getLogger("presidio")
-logger.setLevel(logging.ERROR)
-
-
-class OutputLogger:
-    """Redirect logs.
-
-    https://johnpaton.net/posts/redirect-logging/
-    """
-
-    def __init__(self, name="root", level="INFO"):
-        self.logger = logging.getLogger(name)
-        self.name = self.logger.name
-        self.level = getattr(logging, level)
-        self._redirector = contextlib.redirect_stdout(self)
-
-    def write(self, msg):
-        """Log messages."""
-        if msg and not msg.isspace():
-            self.logger.log(self.level, msg)
-
-    def __enter__(self):
-        self._redirector.__enter__()
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        """Contextlib exception handling."""
-        self._redirector.__exit__(exc_type, exc_value, traceback)
-
-
-with OutputLogger("presidio", "WARN") as redirector:
-    engine = presidio_analyzer.AnalyzerEngine(
-        default_score_threshold=_DEFAULT_SCORE_THRESHOLD, enable_trace_pii=True
-    )
-
-set_config({"sensitive_data.engine": engine})
+from data_describe.config._config import get_option
 
 _DEFAULT_SCORE_THRESHOLD = get_option("sensitive_data.score_threshold")
 _SAMPLE_SIZE = get_option("sensitive_data.sample_size")
--- data_describe/privacy/engine.py
+++ data_describe/privacy/engine.py
@@ -1,44 +1,44 @@
-# import contextlib
-# import logging
+import contextlib
+import logging
 
-# from data_describe.compat import presidio_analyzer
-# from data_describe.config._config import set_config, get_option
-
-
-# set_config({"sensitive_data.sample_size": 100, "sensitive_data.score_threshold": 0.2})
-# _DEFAULT_SCORE_THRESHOLD = get_option("sensitive_data.score_threshold")
-
-# logger = logging.getLogger("presidio")
-# logger.setLevel(logging.ERROR)
+from data_describe.compat import presidio_analyzer
+from data_describe.config._config import set_config, get_option
 
 
-# class OutputLogger:
-#     """Redirect logs.
+set_config({"sensitive_data.sample_size": 100, "sensitive_data.score_threshold": 0.2})
+_DEFAULT_SCORE_THRESHOLD = get_option("sensitive_data.score_threshold")
 
-#     https://johnpaton.net/posts/redirect-logging/
-#     """
-
-#     def __init__(self, name="root", level="INFO"):
-#         self.logger = logging.getLogger(name)
-#         self.name = self.logger.name
-#         self.level = getattr(logging, level)
-#         self._redirector = contextlib.redirect_stdout(self)
-
-#     def write(self, msg):
-#         """Log messages."""
-#         if msg and not msg.isspace():
-#             self.logger.log(self.level, msg)
-
-#     def __enter__(self):
-#         self._redirector.__enter__()
-#         return self
-
-#     def __exit__(self, exc_type, exc_value, traceback):
-#         """Contextlib exception handling."""
-#         self._redirector.__exit__(exc_type, exc_value, traceback)
+logger = logging.getLogger("presidio")
+logger.setLevel(logging.ERROR)
 
 
-# with OutputLogger("presidio", "WARN") as redirector:
-#     engine = presidio_analyzer.AnalyzerEngine(
-#         default_score_threshold=_DEFAULT_SCORE_THRESHOLD, enable_trace_pii=True
-#     )
+class OutputLogger:
+    """Redirect logs.
+
+    https://johnpaton.net/posts/redirect-logging/
+    """
+
+    def __init__(self, name="root", level="INFO"):
+        self.logger = logging.getLogger(name)
+        self.name = self.logger.name
+        self.level = getattr(logging, level)
+        self._redirector = contextlib.redirect_stdout(self)
+
+    def write(self, msg):
+        """Log messages."""
+        if msg and not msg.isspace():
+            self.logger.log(self.level, msg)
+
+    def __enter__(self):
+        self._redirector.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Contextlib exception handling."""
+        self._redirector.__exit__(exc_type, exc_value, traceback)
+
+
+with OutputLogger("presidio", "WARN") as redirector:
+    engine = presidio_analyzer.AnalyzerEngine(
+        default_score_threshold=_DEFAULT_SCORE_THRESHOLD, enable_trace_pii=True
+    )
