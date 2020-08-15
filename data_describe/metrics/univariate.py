17ba6be88534027f76de4e09cdb8dacb5fe823d7
David Law <dvdlaw@gmail.com>
2020-08-15 14:00:40 -0500

    Add NA handling for numpy arrays

--- data_describe/metrics/univariate.py
+++ data_describe/metrics/univariate.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.stats import skew
 
+from data_describe import compat
+
 
 def spikey(data):
     """Calculates the "spikey-ness" of the histogram.
@@ -10,6 +12,10 @@ def spikey(data):
     Args:
         data: The 1-d data array
     """
+    if isinstance(data, compat._SERIES_TYPE):
+        data = data.dropna()
+    else:
+        data = data[~np.isnan(data)]
     counts, bins = np.histogram(data.dropna(), bins="sturges")
     return max(counts) / np.mean(counts)
 
