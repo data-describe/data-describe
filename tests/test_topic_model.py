05e7a0c5e9ed2556d850174911e4df6594483ff3
Rishi Sheth <rishisheth@MWMac-RSHETH.local>
2020-08-17 18:43:24 -0500

    Add public function to create and fit TopicModelWidget

--- tests/test_topic_model.py
+++ tests/test_topic_model.py
@@ -5,7 +5,7 @@ import matplotlib
 import sklearn
 import pytest
 
-from data_describe.text.topic_model import TopicModelWidget
+from data_describe.text.topic_model import topic_model
 
 
 @pytest.fixture(autouse=True)
@@ -82,35 +82,31 @@ def skip_models(monkeypatch):
 
 @pytest.fixture
 def lda_model(document_data):
-    lda_model = TopicModelWidget()
-    lda_model.fit(document_data, max_topics=6, no_below=1, no_above=0.8)
+    lda_model = topic_model(document_data, max_topics=6, no_below=1, no_above=0.8)
     return lda_model
 
 
 @pytest.fixture
 def lsi_model(document_data):
-    lsi_model = TopicModelWidget(model_type="LSI")
-    lsi_model.fit(document_data, max_topics=6, no_below=1, no_above=0.8)
+    lsi_model = topic_model(document_data, model_type="LSI", max_topics=6, no_below=1, no_above=0.8)
     return lsi_model
 
 
 @pytest.fixture
 def svd_model(document_data):
-    svd_model = TopicModelWidget(model_type="SVD", num_topics=5)
-    svd_model.fit(document_data, no_below=1, no_above=0.8)
+    svd_model = topic_model(document_data, model_type="SVD", num_topics=5, no_below=1, no_above=0.8)
     return svd_model
 
 
 @pytest.fixture
 def nmf_model(document_data):
-    nmf_model = TopicModelWidget(model_type="NMF")
-    nmf_model.fit(document_data, tfidf=False, no_below=1, no_above=0.8)
+    nmf_model = topic_model(document_data, model_type="NMF", tfidf=False, no_below=1, no_above=0.8)
     return nmf_model
 
 
 def test_unknown_type():
     with pytest.raises(ValueError):
-        TopicModelWidget("wrong input")
+        topic_model("wrong input")
 
 
 def test_lda_attributes(lda_model):
