acf0f940b73cbe954dcf41caf8896871597752ec
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.fios-router.home>
2020-08-19 10:05:30 -0500

    Fix syntax

--- data_describe/backends/compute/_pandas/__init__.py
+++ data_describe/backends/compute/_pandas/__init__.py
@@ -26,7 +26,8 @@ from data_describe.backends.compute._pandas.dimensionality_reduction import (  #
 )
 from data_describe.backends.compute._pandas.cluster import compute_cluster  # noqa: F401
 from data_describe.backends.compute._pandas.distribution import (  # noqa: F401
-    compute_distribution
+    compute_distribution,
+)
 from data_describe.backends.compute._pandas.correlation_matrix import (  # noqa: F401
     compute_correlation_matrix,
 )
