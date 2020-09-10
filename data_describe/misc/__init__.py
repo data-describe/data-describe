f16f05c0a77b5dac5a85b29fe26a76691f16ac21
David Law <dvdlaw@gmail.com>
2020-09-09 19:51:23 -0500

    Add new utilities for custom colormap

--- data_describe/misc/__init__.py
+++
@@ -1,2 +0,0 @@
-from data_describe.misc.colorscale import rgb_to_str, color_fade  # noqa: F401
-from data_describe.misc.load_data import load_data  # noqa: F401
---
+++ data_describe/misc/colors.py
@@ -0,0 +1,42 @@
+from typing import List
+
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap, Normalize
+from matplotlib.colors import colorConverter
+
+
+def get_p_RdBl_cmap() -> LinearSegmentedColormap:
+    """p_RdBl red-blue colormap."""
+    cdict = {
+        "red": [(0.0, 217, 217), (0.5, 242, 242), (1.0, 65, 65)],
+        "green": [(0.0, 58, 58), (0.5, 242, 242), (1.0, 124, 124)],
+        "blue": [(0.0, 70, 70), (0.5, 242, 242), (1.0, 167, 167)],
+    }
+    # Normalize
+    n_cdict = {
+        color: [(x[0], x[1] / 256.0, x[2] / 256.0) for x in scale]
+        for color, scale in cdict.items()
+    }
+    return LinearSegmentedColormap("p_RdBl", n_cdict)
+
+
+def mpl_to_plotly_cmap(cmap) -> List:
+    """Convert a matplotlib cmap for use with Plotly.
+
+    https://plotly.com/python/v3/matplotlib-colorscales/
+
+    Args:
+        cmap: The matplotlib colormap
+    """
+    pl_rgb = []
+    norm = Normalize(vmin=0, vmax=255)
+    for i in range(0, 255):
+        k = colorConverter.to_rgb(cmap(norm(i)))
+        pl_rgb.append(k)
+    pl_entries = 255
+    h = 1.0 / (pl_entries - 1)
+    pl_colorscale = []
+    for k in range(pl_entries):
+        C = list(map(np.uint8, np.array(cmap(k * h)[:3]) * 255))
+        pl_colorscale.append([k * h, "rgb" + str((C[0], C[1], C[2]))])
+    return pl_colorscale
