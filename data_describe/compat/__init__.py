681c5115bc481e154af5f9f470ef3a35e6ec37d7
David Law <dvdlaw@gmail.com>
2020-10-05 18:22:17 -0500

    Update compat package docstring

--- data_describe/compat/__init__.py
+++ data_describe/compat/__init__.py
@@ -1,3 +1,12 @@
+"""data-describe compatibility and checks.
+
+data-describe makes use of a wide range of Python dependencies that
+are not applicable (and thus not required) for all users. The logic for
+handling these optional dependencies (e.g. lazy-loading) is implemented here.
+
+Note:
+    This subpackage is typically not used by end users of data-describe.
+"""
 from data_describe.compat._dependency import _compat, requires  # noqa: F401
 from data_describe.compat._dataframe import (  # noqa: F401
     _DATAFRAME_BACKENDS,
