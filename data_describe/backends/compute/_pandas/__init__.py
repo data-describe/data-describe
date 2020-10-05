7c56abc7c9ec186c607f83223c34a9b7f32eb1c4
David Law <David Law david.law@mavenwave.com>
2020-10-05 15:15:30 -0500

    Fix failed rebase

---
+++ data_describe/backends/compute/_pandas/__init__.py
@@ -0,0 +1,33 @@
+from data_describe.core.data_heatmap import (  # noqa: F401
+    _pandas_compute_data_heatmap as compute_data_heatmap,
+)
+from data_describe.core.scatter_plot import (  # noqa: F401
+    _pandas_compute_scatter_plot as compute_scatter_plot,
+)
+from data_describe.core.importance import (  # noqa: F401
+    _pandas_compute_importance as compute_importance,
+)
+from data_describe.core.time_series import (  # noqa: F401
+    _pandas_compute_stationarity_test as compute_stationarity_test,
+    _pandas_compute_decompose_timeseries as compute_decompose_timeseries,
+    _pandas_compute_autocorrelation as compute_autocorrelation,
+)
+from data_describe.privacy.detection import compute_sensitive_data  # noqa: F401
+from data_describe.core.summary import (  # noqa: F401
+    _pandas_compute_data_summary as compute_data_summary,
+)
+from data_describe.dimensionality_reduction.dimensionality_reduction import (  # noqa: F401
+    _pandas_compute_run_pca as compute_run_pca,
+    _pandas_compute_run_ipca as compute_run_ipca,
+    _pandas_compute_run_tsne as compute_run_tsne,
+    _pandas_compute_run_tsvd as compute_run_tsvd,
+)
+from data_describe.core.clusters import (  # noqa: F401
+    _pandas_compute_cluster as compute_cluster,
+)
+from data_describe.core.distributions import (  # noqa: F401
+    _pandas_compute_distribution as compute_distribution,
+)
+from data_describe.core.correlations import (  # noqa: F401
+    _pandas_compute_correlation_matrix as compute_correlation_matrix,
+)
