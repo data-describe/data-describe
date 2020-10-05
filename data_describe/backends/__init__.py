39cce39b2432b1d3cf7573d6176be4ee7df74370
David Law <dvdlaw@gmail.com>
2020-10-05 18:31:29 -0500

    Create note section in backend docstring

--- data_describe/backends/__init__.py
+++ data_describe/backends/__init__.py
@@ -1,13 +1,14 @@
 """data-describe backends.
 
-This subpackage is typically not used by end users of data-describe.
-
 `backends` implements a pluggable architecture for the data-describe package.
 This allows third-party libraries to provide alternative implementation details
 for data-describe: for example, implementing Bokeh plot (visualizations, abbreviated
 as `viz`) or implementing calculations on compute clusters (`compute`).
 
 Default implementations in data-describe are also exposed as plugins to this backend system.
+
+Note:
+    This subpackage is typically not used by end users of data-describe.
 """
 from data_describe.backends._backends import (  # noqa
     _get_viz_backend,
