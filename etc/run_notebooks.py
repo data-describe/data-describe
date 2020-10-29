1c7c4aeefad04d659ab8701bad3be9012f0c71a6
David Law <dvdlaw@gmail.com>
2020-10-28 22:05:13 -0500

    Fix execution script and re-run all notebooks

--- etc/run_notebooks.py
+++ etc/run_notebooks.py
@@ -1,4 +1,5 @@
-import glob
+from pathlib import Path
+import logging
 import json
 
 import papermill as pm
@@ -6,14 +7,24 @@ import papermill as pm
 
 def run_all_notebooks():
     """Run all notebooks in the example directory."""
-    for notebook in glob.glob("../examples/*.ipynb"):
-        nb = pm.execute_notebook(notebook, notebook, request_save_on_cell_execute=True)
+    for notebook in Path(__file__).parent.parent.glob("examples/*.ipynb"):
+        notebook_path = str(notebook.resolve())
+        nb = pm.execute_notebook(
+            notebook_path,
+            notebook_path,
+            request_save_on_cell_execute=True,
+            kernel_name="python3",
+        )
 
-        if nb["metadata"]["kernelspec"]["display_name"] != "Python 3":
+        try:
             nb["metadata"]["kernelspec"]["display_name"] = "Python 3"
-            with open(notebook, "w") as fp:
-                json.dump(nb, fp)
+            nb["metadata"]["kernelspec"]["name"] = "python3"
+        except KeyError:
+            pass
+        with open(notebook, "w") as fp:
+            json.dump(nb, fp)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     run_all_notebooks()
