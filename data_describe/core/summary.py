a9db1d88b95d01205e44042537c278843f9465b3
David Law <dvdlaw@gmail.com>
2020-11-07 21:14:24 -0600

    Update data summary pandas calculation and display

--- data_describe/core/summary.py
+++ data_describe/core/summary.py
@@ -1,4 +1,9 @@
+from typing import Dict, Callable
+
 import pandas as pd
+from pandas.core.dtypes.common import is_float
+from pandas.io.formats.info import _sizeof_fmt
+import numpy as np
 
 from data_describe._widget import BaseWidget
 from data_describe.compat import _requires, _compat, _is_series, _is_dataframe
@@ -19,6 +24,7 @@ class SummaryWidget(BaseWidget):
     def __init__(
         self,
         input_data=None,
+        info=None,
         summary=None,
         **kwargs,
     ):
@@ -26,10 +32,12 @@ class SummaryWidget(BaseWidget):
 
         Args:
             input_data: The input data.
+            info: Information about the data shape and size.
             summary: The summary statistics.
         """
         super(SummaryWidget, self).__init__(**kwargs)
         self.input_data = input_data
+        self.info = info
         self.summary = summary
 
     def __str__(self):
@@ -38,40 +46,70 @@ class SummaryWidget(BaseWidget):
     def __repr__(self):
         return "data-describe Summary Widget"
 
-    def show(self, viz_backend=None, **kwargs):
+    def show(
+        self, viz_backend=None, as_percentage: bool = False, auto_float=True, **kwargs
+    ):
         """The default display for this output.
 
-        Returns the summary data as a Pandas dataframe.
+        Displays the summary information.
 
         Args:
             viz_backend: The visualization backend.
+            as_percentage (bool): If True, display counts as percentage over total
+            auto_float (bool): If True, apply formatting to float values
             **kwargs: Keyword arguments.
-
-        Raises:
-            ValueError: Summary data is missing.
 
         Returns:
             The correlation matrix plot.
         """
-        if self.summary is None:
-            raise ValueError("Could not find data to visualize.")
+        try:
+            from IPython.display import display
 
-        return self.summary
+            view = display
+        except ImportError:
+            view = print
+
+        view(self.info)
+
+        summary = self.summary
+        format_dict: Dict[str, Callable] = {}
+
+        as_percentage = as_percentage or self.as_percentage
+        if as_percentage:
+            for col in ["Zeros", "Nulls", "Top Frequency"]:
+                summary[col] = summary[col] / self.input_data.shape[0]
+                format_dict[col] = "{:.1%}".format
+
+        summary.fillna("", inplace=True)
+        auto_float = auto_float or self.auto_float
+        if auto_float:
+            for col in summary.columns:
+                if col not in format_dict.keys():
+                    format_dict[col] = _value_formatter
+
+        view(summary.style.format(format_dict))
 
 
-def data_summary(data, compute_backend=None):
+def data_summary(
+    data, as_percentage: bool = False, auto_float: bool = True, compute_backend=None
+):
     """Summary statistics and data description.
 
     Args:
         data: The dataframe
+        as_percentage (bool): If True, display counts as percentage over total
+        auto_float (bool): If True, apply formatting to float values
         compute_backend: The compute backend.
 
     Returns:
         The dataframe with metrics in rows
     """
-    return _get_compute_backend(backend=compute_backend, df=data).compute_data_summary(
-        data
-    )
+    widget = _get_compute_backend(
+        backend=compute_backend, df=data
+    ).compute_data_summary(data)
+    widget.as_percentage = as_percentage
+    widget.auto_float = auto_float
+    return widget
 
 
 def _count_zeros(series):
@@ -96,6 +134,19 @@ def _count_nulls(series):
         Number of null values
     """
     return series.isnull().sum()
+
+
+def mode1(x):
+    """Mode (counts only) by Warren Weckesser.
+
+    https://stackoverflow.com/questions/46365859/what-is-the-fastest-way-to-get-the-mode-of-a-numpy-array
+
+    Args:
+        x: Input array
+    """
+    _, counts = np.unique(x, return_counts=True)
+    m = counts.argmax()
+    return counts[m]
 
 
 def _most_frequent(series):
@@ -131,39 +182,91 @@ def _pandas_compute_data_summary(data):
     if not _is_dataframe(data):
         raise ValueError("Data must be a Pandas DataFrame")
 
-    # Save column order
-    columns = data.columns
-    dtypes = data.agg([lambda x: x.dtype])
-    moments = data.agg(["mean", "std", "median"])
-    minmax = data.select_dtypes("number").agg(["min", "max"]).reindex(columns=columns)
-    zeros = data.select_dtypes("number").agg([_count_zeros]).reindex(columns=columns)
-    null_summary = data.agg([_count_nulls])
-    freq_summary = data.agg([_most_frequent])
-
-    summary = (
-        dtypes.append(moments, ignore_index=True)
-        .append(minmax, ignore_index=True)
-        .append(zeros, ignore_index=True)
-        .append(null_summary, ignore_index=True)
-        .append(freq_summary, ignore_index=True)
+    info = pd.DataFrame(
+        {
+            "Info": [
+                data.shape[0],
+                data.shape[1],
+                _sizeof_fmt(data.memory_usage().sum(), ""),
+            ]
+        },
+        index=["Rows", "Columns", "Size in Memory"],
     )
-    summary = summary[columns]
-    summary.index = [
-        "Data Type",
-        "Mean",
-        "Standard Deviation",
-        "Median",
-        "Min",
-        "Max",
-        "# Zeros",
-        "# Nulls",
-        "% Most Frequent Value",
-    ]
 
-    # Removing NaNs
-    summary.fillna("", inplace=True)
+    columns = data.columns
+    val = data.values
+    num_columns = data.select_dtypes("number").columns
+    num_ind = np.nonzero([c in num_columns for c in columns])[0]
+    date_columns = data.select_dtypes(["datetime", "datetimetz"]).columns
+    date_ind = np.nonzero([c in date_columns for c in columns])[0]
+    other_columns = data.select_dtypes(
+        exclude=["number", "datetime", "datetimetz"]
+    ).columns
+    other_ind = np.nonzero([c in other_columns for c in columns])[0]
+    order = np.concatenate([num_ind, date_ind, other_ind], axis=0)
 
-    return SummaryWidget(data, summary)
+    dtypes = data.dtypes[order]
+    s_mean = np.pad(
+        np.mean(val[:, num_ind], axis=0),
+        (0, len(data.columns) - num_ind.size),
+        constant_values=np.nan,
+    )
+    s_sd = np.pad(
+        np.std(val[:, num_ind].astype(np.float), axis=0),
+        (0, len(data.columns) - num_ind.size),
+        constant_values=np.nan,
+    )
+    s_med = np.pad(
+        np.median(val[:, num_ind], axis=0),
+        (0, len(data.columns) - num_ind.size),
+        constant_values=np.nan,
+    )
+    s_min = np.pad(
+        np.min(val[:, np.concatenate([num_ind, date_ind])], axis=0),
+        (0, len(data.columns) - num_ind.size - date_ind.size),
+        constant_values=np.nan,
+    )
+    s_max = np.pad(
+        np.max(val[:, np.concatenate([num_ind, date_ind])], axis=0),
+        (0, len(data.columns) - num_ind.size - date_ind.size),
+        constant_values=np.nan,
+    )
+    s_zero = data[data == 0].fillna(0).sum().astype(int)[order]
+    s_null = data.isnull().sum().astype(int)[order]
+    s_unique = data.nunique()[order]
+    s_freq = np.apply_along_axis(mode1, 0, val.astype("str"))[order]
+
+    summary = pd.DataFrame(
+        np.vstack(
+            [
+                dtypes,
+                s_null,
+                s_zero,
+                s_min,
+                s_med,
+                s_max,
+                s_mean,
+                s_sd,
+                s_unique,
+                s_freq,
+            ]
+        ).transpose()[np.argsort(order), :],
+        columns=[
+            "Data Type",
+            "Nulls",
+            "Zeros",
+            "Min",
+            "Median",
+            "Max",
+            "Mean",
+            "Standard Deviation",
+            "Unique",
+            "Top Frequency",
+        ],
+        index=data.columns,
+    )
+
+    return SummaryWidget(data, info, summary)
 
 
 @_requires("modin")
@@ -209,12 +312,46 @@ def _modin_compute_data_summary(data):
         "Median",
         "Min",
         "Max",
-        "# Zeros",
-        "# Nulls",
-        "% Most Frequent Value",
+        "Zeros",
+        "Nulls",
+        "Top Frequency",
     ]
 
-    # Removing NaNs
-    summary.fillna("", inplace=True)
-
     return SummaryWidget(data, summary)
+
+
+def _get_precision(x, margin: int = 1) -> int:
+    """Get the minimum precision for a value.
+
+    Used to determine how to display and format floats.
+
+    Args:
+        x: Input value
+        margin (int): Added to the calculated precision
+
+    Returns:
+        int: Number of decimal places
+    """
+    try:
+        x = (x - np.trunc(x)).astype(np.float)
+        magnitude = np.log10(x[x != 0])
+        return int(np.ceil(np.abs(magnitude)))
+    except TypeError:
+        return 0
+
+
+def _value_formatter(x, precision=None):
+    """Formatter for displaying values of mixed type and precision.
+
+    Args:
+        x: Input value
+        precision (int, optional): Precision for floats.
+    """
+    if is_float(x):
+        precision = precision or _get_precision(x)
+        try:
+            return f"{{:.{precision}f}}".format(x)
+        except ValueError:
+            pass
+    else:
+        return x
