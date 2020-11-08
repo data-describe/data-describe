6b8e942045c0b0cdbea95eb1a9c53c4d2a99e82a
David Law <dvdlaw@gmail.com>
2020-11-08 11:58:31 -0600

    Add additional attribute tests

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -15,17 +15,26 @@ def load_series_summary(compute_backend_df):
 
 
 @pytest.mark.base
-def test_type(load_summary):
+def test_dataframe_attributes(load_summary):
     assert isinstance(load_summary, SummaryWidget)
-    assert _is_dataframe(load_summary.summary)
+    assert _is_dataframe(load_summary.input_data)
+    assert _is_dataframe(load_summary.info_data)
+    assert _is_dataframe(load_summary.summary_data)
+    assert hasattr(load_summary, "as_percentage")
+    assert hasattr(load_summary, "auto_float")
 
 
 @pytest.mark.base
-def test_series(load_series_summary):
+def test_series_attributes(load_series_summary):
     assert isinstance(load_series_summary, SummaryWidget)
-    assert _is_dataframe(load_series_summary.summary)
+    assert _is_dataframe(load_series_summary.input_data)
+    assert _is_dataframe(load_series_summary.info_data)
+    assert _is_dataframe(load_series_summary.summary_data)
+    assert hasattr(load_series_summary, "as_percentage")
+    assert hasattr(load_series_summary, "auto_float")
 
 
 @pytest.mark.base
 def test_shape(load_summary, compute_backend_df):
-    assert load_summary.summary.shape == (compute_backend_df.shape[1], 10)
+    assert load_summary.summary_data.shape == (compute_backend_df.shape[1], 10)
+    assert load_summary.info_data.shape == (3, 1)
