389892b4de17456ee7f752222190dc0b163f0d43
David Law <dvdlaw@gmail.com>
2020-11-11 15:41:19 -0600

    Reworked data summary (#383)
    * Update data summary pandas calculation and display
    
    * Update attributes for summarywidget
    
    * Update test for summary
    
    * Add modin updated implementation
    
    * Add additional attribute tests
    
    * Update modin implementation
    
    * Filter warning on get_precision
    
    * Update notebooks
    
    * Update type hints
    
    * Add margin to precision to show one more digit
    
    * Rerun notebooks
    
    * Add test for no IPython env
    
    * Remove old aggregator functions
    
    * Fix test for __str__

--- data_describe/core/summary.py
+++ data_describe/core/summary.py
@@ -1,4 +1,10 @@
+from typing import Dict, Callable, Optional
+import warnings
+
 import pandas as pd
+from pandas.core.dtypes.common import is_float
+from pandas.io.formats.info import _sizeof_fmt
+import numpy as np
 
 from data_describe._widget import BaseWidget
 from data_describe.compat import _requires, _compat, _is_series, _is_dataframe
@@ -13,24 +19,36 @@ class SummaryWidget(BaseWidget):
 
     Attributes:
         input_data: The input data.
-        summary (DataFrame): The summary statistics.
+        info_data: Information about the data shape and size.
+        summary_data (DataFrame): The summary statistics.
+        as_percentage (bool): If True, display counts as percentage over total
+        auto_float (bool): If True, apply formatting to float values
     """
 
     def __init__(
         self,
         input_data=None,
-        summary=None,
+        info_data=None,
+        summary_data=None,
+        as_percentage: Optional[bool] = False,
+        auto_float: Optional[bool] = True,
         **kwargs,
     ):
         """Data heatmap.
 
         Args:
             input_data: The input data.
-            summary: The summary statistics.
+            info_data: Information about the data shape and size.
+            summary_data: The summary statistics.
+            as_percentage (bool): If True, display counts as percentage over total
+            auto_float (bool): If True, apply formatting to float values
         """
         super(SummaryWidget, self).__init__(**kwargs)
         self.input_data = input_data
-        self.summary = summary
+        self.info_data = info_data
+        self.summary_data = summary_data
+        self.as_percentage = as_percentage
+        self.auto_float = auto_float
 
     def __str__(self):
         return "data-describe Summary Widget"
@@ -38,79 +56,90 @@ class SummaryWidget(BaseWidget):
     def __repr__(self):
         return "data-describe Summary Widget"
 
-    def show(self, viz_backend=None, **kwargs):
+    def show(
+        self,
+        viz_backend=None,
+        as_percentage: Optional[bool] = None,
+        auto_float: Optional[bool] = None,
+        **kwargs,
+    ):
         """The default display for this output.
 
-        Returns the summary data as a Pandas dataframe.
+        Displays the summary information.
 
         Args:
             viz_backend: The visualization backend.
+            as_percentage (bool): If True, display counts as percentage over total
+            auto_float (bool): If True, apply formatting to float values
             **kwargs: Keyword arguments.
-
-        Raises:
-            ValueError: Summary data is missing.
 
         Returns:
             The correlation matrix plot.
         """
-        if self.summary is None:
-            raise ValueError("Could not find data to visualize.")
+        try:
+            from IPython.display import display
 
-        return self.summary
+            view = display
+        except ImportError:
+            view = print
+
+        view(self.info_data)
+
+        summary_data = self.summary_data
+        format_dict: Dict[str, Callable] = {}
+
+        as_percentage = as_percentage or self.as_percentage
+        if as_percentage:
+            for col in ["Zeros", "Nulls", "Top Frequency"]:
+                summary_data[col] = summary_data[col] / self.input_data.shape[0]
+                format_dict[col] = "{:.1%}".format
+
+        summary_data.fillna("", inplace=True)
+        auto_float = auto_float or self.auto_float
+        if auto_float:
+            for col in summary_data.columns:
+                if col not in format_dict.keys():
+                    format_dict[col] = _value_formatter
+
+        if len(format_dict) > 0:
+            view(summary_data.style.format(format_dict))
+        else:
+            view(summary_data)
 
 
-def data_summary(data, compute_backend=None):
+def data_summary(
+    data, as_percentage: bool = False, auto_float: bool = True, compute_backend=None
+):
     """Summary statistics and data description.
 
     Args:
         data: The dataframe
+        as_percentage (bool): If True, display counts as percentage over total
+        auto_float (bool): If True, apply formatting to float values
         compute_backend: The compute backend.
 
     Returns:
         The dataframe with metrics in rows
     """
-    return _get_compute_backend(backend=compute_backend, df=data).compute_data_summary(
-        data
-    )
+    widget = _get_compute_backend(
+        backend=compute_backend, df=data
+    ).compute_data_summary(data)
+    widget.as_percentage = as_percentage
+    widget.auto_float = auto_float
+    return widget
 
 
-def _count_zeros(series):
-    """Count of zero values in a Pandas or Modin series.
+def mode1(x):
+    """Mode (counts only) by Warren Weckesser.
 
-    Args:
-        series: The Pandas or Modin series
-
-    Returns:
-        Number of zeros
-    """
-    return (series == 0).sum()
-
-
-def _count_nulls(series):
-    """Count of null values in a Pandas or Modin series.
+    https://stackoverflow.com/questions/46365859/what-is-the-fastest-way-to-get-the-mode-of-a-numpy-array
 
     Args:
-        series: The Pandas or Modin series
-
-    Returns:
-        Number of null values
+        x: Input array
     """
-    return series.isnull().sum()
-
-
-def _most_frequent(series):
-    """Percent of most frequent value, per column, in a Pandas or Modin data frame.
-
-    Args:
-        series: The Pandas or Modin series
-
-    Returns:
-        Percent of most frequent value per column
-    """
-    counts = series.value_counts()
-    if counts.shape[0] == 0:
-        return None
-    return round(counts.iloc[0] / series.shape[0] * 100, 2)
+    _, counts = np.unique(x, return_counts=True)
+    m = counts.argmax()
+    return counts[m]
 
 
 def _pandas_compute_data_summary(data):
@@ -131,39 +160,91 @@ def _pandas_compute_data_summary(data):
     if not _is_dataframe(data):
         raise ValueError("Data must be a Pandas DataFrame")
 
-    # Save column order
-    columns = data.columns
-    dtypes = data.agg([lambda x: x.dtype])
-    moments = data.agg(["mean", "std", "median"])
-    minmax = data.select_dtypes("number").agg(["min", "max"]).reindex(columns=columns)
-    zeros = data.select_dtypes("number").agg([_count_zeros]).reindex(columns=columns)
-    null_summary = data.agg([_count_nulls])
-    freq_summary = data.agg([_most_frequent])
-
-    summary = (
-        dtypes.append(moments, ignore_index=True)
-        .append(minmax, ignore_index=True)
-        .append(zeros, ignore_index=True)
-        .append(null_summary, ignore_index=True)
-        .append(freq_summary, ignore_index=True)
+    info_data = pd.DataFrame(
+        {
+            "Info": [
+                data.shape[0],
+                data.shape[1],
+                _sizeof_fmt(data.memory_usage().sum(), ""),
+            ]
+        },
+        index=["Rows", "Columns", "Size in Memory"],
     )
-    summary = summary[columns]
-    summary.index = [
-        "Data Type",
-        "Mean",
-        "Standard Deviation",
-        "Median",
-        "Min",
-        "Max",
-        "# Zeros",
-        "# Nulls",
-        "% Most Frequent Value",
-    ]
 
-    # Removing NaNs
-    summary.fillna("", inplace=True)
+    columns = data.columns
+    val = data.values
+    num_columns = data.select_dtypes("number").columns
+    num_ind = np.nonzero([c in num_columns for c in columns])[0]
+    date_columns = data.select_dtypes(["datetime", "datetimetz"]).columns
+    date_ind = np.nonzero([c in date_columns for c in columns])[0]
+    other_columns = data.select_dtypes(
+        exclude=["number", "datetime", "datetimetz"]
+    ).columns
+    other_ind = np.nonzero([c in other_columns for c in columns])[0]
+    order = np.concatenate([num_ind, date_ind, other_ind], axis=0)
 
-    return SummaryWidget(data, summary)
+    dtypes = data.dtypes[order]
+    s_mean = np.pad(
+        np.mean(val[:, num_ind], axis=0),
+        (0, len(data.columns) - num_ind.size),
+        constant_values=np.nan,
+    )
+    s_sd = np.pad(
+        np.std(val[:, num_ind].astype(np.float), axis=0),
+        (0, len(data.columns) - num_ind.size),
+        constant_values=np.nan,
+    )
+    s_med = np.pad(
+        np.median(val[:, num_ind], axis=0),
+        (0, len(data.columns) - num_ind.size),
+        constant_values=np.nan,
+    )
+    s_min = np.pad(
+        np.min(val[:, np.concatenate([num_ind, date_ind])], axis=0),
+        (0, len(data.columns) - num_ind.size - date_ind.size),
+        constant_values=np.nan,
+    )
+    s_max = np.pad(
+        np.max(val[:, np.concatenate([num_ind, date_ind])], axis=0),
+        (0, len(data.columns) - num_ind.size - date_ind.size),
+        constant_values=np.nan,
+    )
+    s_zero = data[data == 0].fillna(0).sum().astype(int)[order]
+    s_null = data.isnull().sum().astype(int)[order]
+    s_unique = data.nunique()[order]
+    s_freq = np.apply_along_axis(mode1, 0, val.astype("str"))[order]
+
+    summary_data = pd.DataFrame(
+        np.vstack(
+            [
+                dtypes,
+                s_null,
+                s_zero,
+                s_min,
+                s_med,
+                s_max,
+                s_mean,
+                s_sd,
+                s_unique,
+                s_freq,
+            ]
+        ).transpose()[np.argsort(order), :],
+        columns=[
+            "Data Type",
+            "Nulls",
+            "Zeros",
+            "Min",
+            "Median",
+            "Max",
+            "Mean",
+            "Standard Deviation",
+            "Unique",
+            "Top Frequency",
+        ],
+        index=data.columns,
+    )
+
+    return SummaryWidget(data, info_data, summary_data)
 
 
 @_requires("modin")
@@ -185,36 +266,104 @@ def _modin_compute_data_summary(data):
     if not _is_dataframe(data):
         raise ValueError("Data must be a Modin DataFrame")
 
+    info_data = pd.DataFrame(
+        {
+            "Info": [
+                data.shape[0],
+                data.shape[1],
+                _sizeof_fmt(data.memory_usage().sum(), ""),
+            ]
+        },
+        index=["Rows", "Columns", "Size in Memory"],
+    )
+
     # Save column order
     columns = data.columns
-    dtypes = data.agg([lambda x: x.dtype])
-    moments = data.agg(["mean", "std", "median"])
-    minmax = data.select_dtypes("number").agg(["min", "max"]).reindex(columns=columns)
-    zeros = data.select_dtypes("number").agg([_count_zeros]).reindex(columns=columns)
-    null_summary = data.agg([_count_nulls])
-    freq_summary = data.agg([_most_frequent])
 
-    summary = (
-        dtypes.append(moments, ignore_index=True)
-        .append(minmax, ignore_index=True)
-        .append(zeros, ignore_index=True)
-        .append(null_summary, ignore_index=True)
-        .append(freq_summary, ignore_index=True)
+    dtypes = data.dtypes.to_numpy()
+    s_mean = data.mean(numeric_only=True).reindex().to_numpy()
+    s_sd = data.std(numeric_only=True).reindex(columns).to_numpy()
+    s_med = data.median(numeric_only=True).reindex(columns).to_numpy()
+    s_min = data.min(numeric_only=True).reindex(columns).to_numpy()
+    s_max = data.max(numeric_only=True).reindex(columns).to_numpy()
+    s_zero = data[data == 0].fillna(0).sum().astype(int).to_numpy()
+    s_null = data.isnull().sum().astype(int).to_numpy()
+    s_unique = data.nunique().to_numpy()
+    s_freq = (
+        data.apply(lambda x: mode1(x.astype("str")))
+        .iloc[
+            0,
+        ]
+        .to_numpy()
     )
-    summary = summary[columns]
-    summary.index = [
-        "Data Type",
-        "Mean",
-        "Standard Deviation",
-        "Median",
-        "Min",
-        "Max",
-        "# Zeros",
-        "# Nulls",
-        "% Most Frequent Value",
-    ]
 
-    # Removing NaNs
-    summary.fillna("", inplace=True)
+    summary_data = pd.DataFrame(
+        np.vstack(
+            [
+                dtypes,
+                s_null,
+                s_zero,
+                s_min,
+                s_med,
+                s_max,
+                s_mean,
+                s_sd,
+                s_unique,
+                s_freq,
+            ]
+        ).transpose(),
+        columns=[
+            "Data Type",
+            "Nulls",
+            "Zeros",
+            "Min",
+            "Median",
+            "Max",
+            "Mean",
+            "Standard Deviation",
+            "Unique",
+            "Top Frequency",
+        ],
+        index=columns,
+    )
 
-    return SummaryWidget(data, summary)
+    return SummaryWidget(data, info_data, summary_data)
+
+
+def _get_precision(x, margin: int = 1) -> int:
+    """Get the minimum precision for a value.
+
+    Used to determine how to display and format floats.
+
+    Args:
+        x: Input value
+        margin (int): Added to the calculated precision
+
+    Returns:
+        int: Number of decimal places
+    """
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
+        try:
+            x = (x - np.trunc(x)).astype(np.float)
+            magnitude = np.log10(x[x != 0])
+            return int(np.ceil(np.abs(magnitude))) + margin
+        except (TypeError, ValueError):
+            return 0
+
+
+def _value_formatter(x, precision=None):
+    """Formatter for displaying values of mixed type and precision.
+
+    Args:
+        x: Input value
+        precision (int, optional): Precision for floats.
+    """
+    if is_float(x):
+        precision = precision or _get_precision(x)
+        try:
+            return f"{{:.{precision}f}}".format(x)
+        except ValueError:
+            pass
+    else:
+        return x
--- tests/test_summary.py
+++ tests/test_summary.py
@@ -1,3 +1,5 @@
+import sys
+
 import pytest
 
 from data_describe.compat import _is_dataframe
@@ -15,17 +17,54 @@ def load_series_summary(compute_backend_df):
 
 
 @pytest.mark.base
-def test_type(load_summary):
+@pytest.mark.parametrize(
+    "compute_backend_df",
+    ["pandas", pytest.param("modin.pandas", marks=pytest.mark.xfail)],
+    indirect=True,
+)  # xfail: modin #2376
+def test_dataframe_attributes(load_summary, compute_backend_df):
     assert isinstance(load_summary, SummaryWidget)
-    assert _is_dataframe(load_summary.summary)
+    assert str(load_summary) == "data-describe Summary Widget"
+    assert _is_dataframe(load_summary.input_data)
+    assert _is_dataframe(load_summary.info_data)
+    assert _is_dataframe(load_summary.summary_data)
+    assert hasattr(load_summary, "as_percentage")
+    assert hasattr(load_summary, "auto_float")
+
+
+@pytest.mark.parametrize(
+    "compute_backend_df",
+    ["pandas"],
+    indirect=True,
+)
+def test_no_ipython(load_summary, compute_backend_df, monkeypatch, capsys):
+    monkeypatch.setitem(sys.modules, "IPython.display", None)
+    load_summary.show()
+    assert "Size in Memory" in capsys.readouterr().out
 
 
 @pytest.mark.base
-def test_series(load_series_summary):
+@pytest.mark.parametrize(
+    "compute_backend_df",
+    ["pandas", pytest.param("modin.pandas", marks=pytest.mark.xfail)],
+    indirect=True,
+)  # xfail: modin #2376
+def test_series_attributes(load_series_summary, compute_backend_df):
     assert isinstance(load_series_summary, SummaryWidget)
-    assert _is_dataframe(load_series_summary.summary)
+    assert str(load_series_summary) == "data-describe Summary Widget"
+    assert _is_dataframe(load_series_summary.input_data)
+    assert _is_dataframe(load_series_summary.info_data)
+    assert _is_dataframe(load_series_summary.summary_data)
+    assert hasattr(load_series_summary, "as_percentage")
+    assert hasattr(load_series_summary, "auto_float")
 
 
 @pytest.mark.base
+@pytest.mark.parametrize(
+    "compute_backend_df",
+    ["pandas", pytest.param("modin.pandas", marks=pytest.mark.xfail)],
+    indirect=True,
+)  # xfail: modin #2376
 def test_shape(load_summary, compute_backend_df):
-    assert load_summary.summary.shape == (9, compute_backend_df.shape[1])
+    assert load_summary.summary_data.shape == (compute_backend_df.shape[1], 10)
+    assert load_summary.info_data.shape == (3, 1)
