471ece8006f28ad2338a360f9ac6fea0bbfe38e0
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2020-11-12 12:16:37 -0600

    Modify test print

--- data_describe/text/topic_modeling.py
+++ data_describe/text/topic_modeling.py
@@ -114,7 +114,7 @@ class TopicModelWidget(BaseWidget):
             )
         self._num_topics = num_topics
         self._model_kwargs = model_kwargs
-        print("Test 1,2,3")
+        print("Test 1,2,3,4")
 
     def __str__(self):
         return "data-describe Topic Model Widget"
