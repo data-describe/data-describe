6c3bd8802d1ab58104bc31d72f9ca77a95199cbd
Richard Truong-Chau <richardtruong-chau@MWMAC-RTRUONG-CHAU.local>
2021-01-29 17:30:04 -0600

    Add _plotly_viz_auto to __init__.py

--- data_describe/backends/viz/_plotly/__init__.py
+++ data_describe/backends/viz/_plotly/__init__.py
@@ -14,4 +14,5 @@ from data_describe.core.correlation import (  # noqa: F401
 )
 from data_describe.anomaly.detection import (  # noqa: F401
     _plotly_viz_anomaly as viz_plot_anomaly,
+    _plotly_viz_auto as viz_plot_auto,
 )
